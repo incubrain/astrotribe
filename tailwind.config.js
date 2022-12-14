3342552be92402ac6f18eab01e9819f56e070a44
Drew MacGibbon <82544364+Drew-Macgibbon@users.noreply.github.com>
2022-12-14 13:43:22 +1300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}'
+  ],
+  mode: 'jit',
+  theme: {
+    extend: {}
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    // https://tailwindcss.com/docs/typography-plugin
+    require('@tailwindcss/forms')
+    // https://github.com/tailwindlabs/tailwindcss-forms
+  ]
+}
