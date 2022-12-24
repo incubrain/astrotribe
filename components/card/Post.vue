73a761513f8a81e264ae7ba8430b56ae9d19bb4f
Drew MacGibbon <drewmacgibbon@gmail.com>
2022-12-24 14:56:04 +1300

    page | single article and routing

--- components/card/Post.vue
+++ components/card/Post.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="grid grid-rows-4 rounded-md shadow-sm overflow-hidden
-        animate-swipe-in scale-x-0 origin-left
+        animate-pop-in scale-x-0 origin-left group
     ">
         <img src="/cover.jpg" class=" row-start-1 row-span-3 col-span-1 col-start-1 object-cover object-top shadow-lg h-[360px]">
         <div class=" text-black row-start-3 row-span-2 w-full col-span-1 col-start-1 relative
         transition-all duration-500 ease-in-out">
-        <div class="p-8 group absolute bottom-0 bg-white">
-            <button class="h-[40px] px-8 bg-[#440439] rounded-md absolute top-[-20px] left-0 right-0 w-[80%] shadow-lg mx-auto text-white font-bold"> read more </button>
+        <div class="p-8 absolute bottom-0 bg-white">
+            <button class="h-[40px] px-8 bg-[#440439] rounded-md absolute top-[-20px] left-0 right-0 w-[80%]
+                shadow-lg mx-auto text-white font-bold
+                before:content-['Category']
+                group-hover:before:content-['Read_more']
+                "></button>
             <h1 class="text-2xl font-semibold">Post title blah blah, over a few lines</h1>
             <p class="text-sm h-[0px] group-hover:h-[160px] overflow-hidden transition-all duration-500 ease-in-out"> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
                 The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', 
---
+++ pages/news/article/[id].vue
@@ -0,0 +1,54 @@
+<template>
+    <div v-if="post">
+        <div class="max-w-[760px] mx-auto flex flex-col prose justify-center">
+            <div>
+                <div class="my-10 lg:my-10 flex flex-col justify-center">
+                    <img :src="post.featuredImage" class="rounded-xl shadow-xl" />
+                    <h1 class="text-4xl lg:text-6xl leading-10 text-center">
+                        {{ post.title }}
+                    </h1>
+                    <div class="flex flex-row gap-2items-center justify-center">
+                        <p class="font-semibold">
+                            Published: {{ post.published }}
+                        </p>
+                    </div>
+                </div>
+                <div class="max-w-[920px] mx-auto">
+                        <p class="m-0">
+                            {{ post.body }}
+                        </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+const posts = [
+    {
+        id: 0, 
+        title: 'Test post', 
+        body: `It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Man`,  
+        featuredImage: '/cover.jpg', 
+        published: '24th December 2022' 
+    },
+    {
+        id: 1, 
+        title: 'Test post 2', 
+        body: `It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Man`,  
+        featuredImage: '/cover.jpg', 
+        published: '24th December 2022' 
+    }
+]
+
+const route = useRoute()
+const path = computed(() => route.path)
+const post = posts[Number(route.path.split('/').at(-1))] || null
+
+// watch(
+//     () => route.path,
+//     () => getProject(),
+//     { deep: true }
+// )
+</script>
