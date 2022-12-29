80c9b7d7d697afc17a20319c2e75fa8d0d4190b2
Drew MacGibbon <drewmacgibbon@gmail.com>
2022-12-29 23:51:09 +1300

    add | formkit, basic testing

---
+++ components/form/Tags.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <FormKit type="form" #default="{ value }" :actions="false">
+            <FormKit
+                type="taglist"
+                name="carBrands"
+                label="Search for your favorite car brands"
+                :options="carBrands"
+                :value="[]"
+            />
+            <pre wrap>{{ value }}</pre>
+        </FormKit>
+    </div>
+</template>
+
+<script setup lang="ts">
+const carBrands = [
+    { label: 'Audi', value: 'audi' },
+    { label: 'BMW', value: 'bmw' },
+    { label: 'Citroen', value: 'citroen' },
+    { label: 'Dacia', value: 'dacia' },
+    { label: 'Fiat', value: 'fiat' },
+    { label: 'Ford', value: 'ford' },
+    { label: 'Ferrari', value: 'ferrari' },
+    { label: 'Honda', value: 'honda' },
+    { label: 'Hyundai', value: 'hyundai' },
+    { label: 'Jaguar', value: 'jaguar' },
+    { label: 'Jeep', value: 'jeep' },
+    { label: 'Kia', value: 'kia' },
+    { label: 'Land Rover', value: 'land rover' },
+    { label: 'Mazda', value: 'mazda' },
+    { label: 'Mercedes', value: 'mercedes' },
+    { label: 'Mini', value: 'mini' },
+    { label: 'Mitsubishi', value: 'mitsubishi' },
+    { label: 'Nissan', value: 'nissan' },
+    { label: 'Opel', value: 'opel' },
+    { label: 'Peugeot', value: 'peugeot' },
+    { label: 'Porsche', value: 'porsche' },
+    { label: 'Renault', value: 'renault' },
+    { label: 'Saab', value: 'saab' },
+    { label: 'Skoda', value: 'skoda' },
+    { label: 'Subaru', value: 'subaru' },
+    { label: 'Suzuki', value: 'suzuki' },
+    { label: 'Toyota', value: 'toyota' },
+    { label: 'Volkswagen', value: 'volkswagen' },
+    { label: 'Volvo', value: 'volvo' },
+]
+</script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,84 @@
 <template>
-    <div class="min-h-full grid 
+    <div
+        class="min-h-full grid 
         grid-cols-[minmax(160px,1fr)]
-        md:grid-cols-[minmax(160px,1fr)_minmax(160px,1fr)]
-        lg:grid-cols-[minmax(160px,1fr)_minmax(160px,1fr)_minmax(160px,1fr)_minmax(160px,1fr)]
-        rounded-md shadow-sm gap-6
-    ">
-        <CardPost />
-        <CardPost />
-        <CardPost />
-        <CardPost />
-        <CardPost />
-        <CardPost />
-        <CardPost />
+        lg:grid-cols-[minmax(100px,.2fr)_minmax(600px,4fr)_minmax(100px,0.2fr)]
+        rounded-md shadow-sm"
+    >
+        <div class="bg-red-200 rounded-lg row-start-1 col-span-1 lg:col-span-3 col-start-1 w-full h-[360px] relative">
+        </div>
+        <div class="bg-white w-full h-[200px] relative col-start-2 row-span-1 row-start-2">
+            <div class="absolute left-0 top-[-120px] w-[240px] h-[240px] rounded-full bg-black border-8 border-white">
+                <SupaUpload />
+            </div>
+
+        </div>
+        <div class="bg-white p-6 shadow-lg flex flex-row gap-6 justify-between row-start-3 col-start-2 w-full">
+            <div class="w-full">
+                <FormKit
+                    label="Username"
+                    type="text"
+                    help="Pick a new username"
+                    validation="required|matches:/^@[a-zA-Z]+$/|length:5"
+                    value="@useFormKit"
+                />
+                <FormKit
+                    type="date"
+                    value="yyyy-mm-dd"
+                    label="Birthday"
+                    help="Enter your birth day"
+                    validation="required|date_before:2010-01-01"
+                    validation-visibility="live"
+                />
+                <FormKit
+                    type="email"
+                    label="Student email address"
+                    help="Please enter your student email address."
+                    validation="required|email|ends_with:.edu"
+                    validation-visibility="live"
+                    placeholder="vikas@school.edu"
+                />
+                <FormKit
+                    type="tel"
+                    label="Phone number"
+                    placeholder="xxx-xxx-xxxx"
+                    validation="matches:/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/"
+                    :validation-messages="{
+                        matches:
+                            'Phone number must be in the format xxx-xxx-xxxx',
+                    }"
+                    validation-visibility="dirty"
+                />
+                <FormKit
+                    type="textarea"
+                    label="Your Essay"
+                    rows="10"
+                    placeholder="Remember to write in complete sentences."
+                    help="I'll know if you didn't read the book!"
+                />
+                <FormKit type="button" help="You can bind event listeners.">
+                    Click me!
+                </FormKit>
+            </div>
+            <!-- <FormSocial /> -->
+            <div class="w-full ">
+                <FormKit
+                    type="checkbox"
+                    label="Toppings"
+                    :options="['Mushrooms', 'Olives', 'Salami', 'Anchovies']"
+                    decorator-icon="happy"
+                    help="Select your pizza toppings"
+                    validation="required|min:3"
+                />
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-
 definePageMeta({ name: 'Home' })
-
 </script>
+
+<style scoped>
+
+</style>
