e798467be9007839d50a0f8496ad59f1527e7a76
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-02-09 13:39:44 +0530

    chore: refactor appStars

---
+++ components/common/CommonCoverImg.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="absolute top-0 left-0 right-0 w-full h-[80vh]">
+        <img src="/cover1.jpg" class="object-cover w-full h-full" />
+        <div
+            class="bg-[#12060625] w-full h-full bg-blend-overlay absolute top-0 left-0"
+        ></div>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
