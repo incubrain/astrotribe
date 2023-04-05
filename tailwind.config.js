928d949748eb86022842a927319032e2a22ecb8e
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-04-06 00:32:07 +0530

    conf: update tailwind screen size xl, 2xl
    also updated the main-grid class

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,10 +29,10 @@ module.exports = {
             lg: '1024px',
             // => @media (min-width: 1024px) { ... }
 
-            xl: '1280px',
+            xl: '1680px',
             // => @media (min-width: 1280px) { ... }
 
-            '2xl': '1536px',
+            '2xl': '2000px',
             // => @media (min-width: 1536px) { ... }
         },
         extend: {
