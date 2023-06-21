261ce475c65ba353c46fd9f5a6ad4319bfd5318a
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-06-21 23:08:18 +0530

    feat(form): button useIsFormDirty/useIsFormValid

---
+++ components/form/FormButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <UButton
+    color="primary"
+    size="md"
+    class="w-full flex items-center justify-center"
+    type="submit"
+    :disabled="isDisabled"
+  >
+    <slot />
+  </UButton>
+</template>
+
+<script setup lang="ts">
+import { useIsFormDirty, useIsFormValid } from 'vee-validate'
+
+const isDirty = useIsFormDirty()
+const isValid = useIsFormValid()
+
+const isDisabled = computed(() => {
+  return !isDirty.value || !isValid.value
+})
+</script>
