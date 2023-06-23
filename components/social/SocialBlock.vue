302361fa72a44ca2d2324051488c34a233eac933
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-06-23 13:05:48 +0530

    chore(refactor): SocialBlock takes socials as prop

--- components/social/SocialBlock.vue
+++ components/social/SocialBlock.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="flex flex-col">
-    <h2 class=" text-xl lg:text-2xl font-semibold leading-normal">Socials</h2>
+    <h2 v-if="hasTitle" class=" text-xl lg:text-2xl font-semibold leading-normal">Follow us</h2>
     <div class="flex w-full gap-2 mt-2">
-      <IconSocial v-for="s in contactData.socials" :key="s.platform" :platform="s.platform" :link="s.link" />
+      <IconSocial v-for="s in socials" :key="s.platform" :platform="s.platform" :link="s.link" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import contactData from '@/data/home/contact.json'
+
+interface Social {
+  platform: string
+  link: string
+}
+
+defineProps({
+  socials: {
+    type: Array as PropType<Social[]>,
+    required: true
+  },
+  hasTitle: {
+    type: Boolean,
+    default: true
+  }
+})
 
 </script>
 
