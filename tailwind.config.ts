803d2134d4b9d95e0e06cafbd1f72b090b109d77
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-06-24 19:33:10 +0530

    chore(package): remove tailwind/forms

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -83,7 +83,5 @@ export default <Partial<Config>>{
   plugins: [
     // require('@tailwindcss/typography'),
     // https://tailwindcss.com/docs/typography-plugin
-    // require('@tailwindcss/forms')
-    // https://github.com/tailwindlabs/tailwindcss-forms
   ]
 }
