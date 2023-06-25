520190e1b51c349ba61b62c56ab3e0f59b2505a3
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-06-25 15:14:22 +0530

    chore(refactor): improved types for getImageURL

--- composables/supabase/get/image.ts
+++ composables/supabase/get/image.ts
@@ -1,69 +1,90 @@
+import { stringIsNull } from '~/composables/utils/strings'
+
+type FileType =
+  | 'venue-logo'
+  | 'venue-featured-image'
+  | 'venue-images'
+  | 'event-featured-image'
+  | 'event-images'
+  | 'user-avatar'
+  | 'user-cover'
+
+// Map this to supabase storage bucket structures
+export const defaultConstructorOptions: Record<FileType, string> = {
+  'venue-logo': 'logo.png',
+  'venue-featured-image': 'featured_image.jpg',
+  'venue-images': 'images/',
+  'event-featured-image': 'featured_image.png',
+  'event-images': 'images/',
+  'user-avatar': 'avatar.png',
+  'user-cover': 'cover.jpg'
+}
+
 interface UrlConstructorOptions {
   baseURL: string
-  bucket?: string
-  folderPath?: string
-  file?: string
+  bucket: 'profile-public' | 'posts' | 'venues'
+  file: string
+  folderPath: string
+  fileType: FileType
   isPrivate?: boolean
-  transform?: { width?: number; height?: number; fit?: string; quality?: number } | null
+  transform?: {
+    width?: number
+    height?: number
+    fit?: 'contain' | 'cover' | 'fill'
+    quality?: number
+  } | null
 }
 
 export const urlConstructor = (options: UrlConstructorOptions) => {
   const {
     baseURL,
-    bucket = 'profile-public',
-    folderPath = 'defaults/avatar',
-    file = 'default.png',
+    bucket,
+    file,
+    folderPath,
+    fileType,
     isPrivate = false,
     transform = null
   } = options
 
-  if (file === 'default.png') {
-    return `${baseURL}${bucket}/defaults/${file}`
+  let filePath = `${folderPath}/${file}`
+  console.log('filePath', fileType, file, stringIsNull(file))
+  if (stringIsNull(file) && fileType) {
+    filePath = `defaults/${defaultConstructorOptions[fileType]}`
   }
 
   const accessType = isPrivate ? 'private' : 'public'
-  const url = new URL(`${accessType}/${bucket}/${folderPath}/${file}`, baseURL)
+  const url = new URL(`${accessType}/${bucket}/${filePath}`, baseURL)
 
   if (transform) {
-    for (const [key, value] of Object.entries(transform)) {
+    Object.entries(transform).forEach(([key, value]) => {
       if (value !== undefined) {
         url.searchParams.append(key, value.toString())
       }
-    }
+    })
   }
 
   return url.href
 }
 
-// interface ImageUrlOptions {
-//   bucket?: string
-//   folderPath?: string
-//   file?: string
-//   isPrivate?: boolean
-//   transform?: { width?: number; height?: number; resize?: string; quality?: number } | null
-// }
+interface GetImageUrlOptions extends Omit<UrlConstructorOptions, 'baseURL'> {}
 
 export const getImageURL = ({
   bucket,
+  file,
+  fileType,
   folderPath,
-  file = 'default.png',
   isPrivate,
   transform
-}: {
-  bucket?: string
-  folderPath?: string
-  file?: string
-  isPrivate?: boolean
-  transform?: { width?: number; height?: number; fit?: string; quality?: number } | null
-}): string => {
+}: GetImageUrlOptions): string => {
   const superbaseURL = useRuntimeConfig().public.SUPABASE_URL
   const baseURL = `${superbaseURL}/storage/v1/render/image/`
 
   return urlConstructor({
     baseURL,
     bucket,
-    folderPath,
     file,
+    fileType,
+    folderPath,
     isPrivate,
     transform
   })
