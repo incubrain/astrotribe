dd84afaed303461fd4b29e2257c8b76cac01063a
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-06-25 23:15:46 +0530

    feat(admin): AdminBulkAddSupabase

---
+++ components/admin/AdminBulkAddSupabase.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="p-8 rounded-md shadow-md foreground">
+    <h1 class="text-xl font-semibold">Admin</h1>
+    <div class="flex flex-wrap gap-2">
+      <UButton
+        color="primary"
+        class="mt-4"
+        @click="createVenues"
+      >
+        Create Venues
+      </UButton>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const createVenues = async () => await useAsyncData(() => $fetch('/api/admin/venues/create-many'))
+
+</script>
+
+<style scoped></style>
--- pages/astrotribe/index.vue
+++ pages/astrotribe/index.vue
@@ -15,23 +15,10 @@
         <UButton color="primary"> Register your interest here. </UButton>
       </NuxtLink>
     </div>
-    <div class="p-8 rounded-md shadow-md foreground">
-      <h1 class="text-xl font-semibold">Admin</h1>
-      <div class="flex flex-wrap gap-2">
-        <UButton
-          color="primary"
-          class="mt-4"
-          @click="createVenues"
-        >
-          Create Venues
-        </UButton>
-      </div>
-    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-const createVenues = async () => await useAsyncData(() => $fetch('/api/admin/venues/create-many'))
 </script>
 
 <style scoped></style>
