622cac9bb9e73c1963c48aa6cdca35be32fdc214
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-06-25 23:11:02 +0530

    fix(admin): turn off admin route

--- server/api/admin/venues/create-many.ts
+++ server/api/admin/venues/create-many.ts
@@ -1,19 +1,23 @@
 import supabaseClient from '../../../utils/supabaseClient'
-import venues from '../../../../private-data/venues.json'
+// import venues from '../../../../private-data/venues.json'
 
 export default cachedEventHandler(async (event) => {
-  const client = supabaseClient()
-  let status: Number, message: String
-  const { data, error } = await client.from('venues').insert(venues)
-  console.log('venues', data, error)
-  if (error) {
-    status = 500
-    message = 'Error creating venues'
-  } else {
-    status = 200
-    message = 'Venues created'
+  const admin = false
+  let status = 400
+  let message = 'You Are Not Authorized'
+  if (admin) {
+    const client = supabaseClient()
+    const { data, error } = await client.from('venues').insert(venues)
+    console.log('venues', data, error)
+    if (error) {
+      status = 500
+      message = 'Error creating venues'
+    } else {
+      status = 200
+      message = 'Venues created'
+    }
+    console.log('venues status', status, message)
   }
-  console.log('venues status', status, message)
   return {
     status,
     message
