bbb7124fdb7c26ee0d03a22b40034a57696b131b
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-06-25 10:58:57 +0530

    chore(alpha): page explaining we're in alpha

--- pages/astrotribe/index.vue
+++ pages/astrotribe/index.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="w-full p-8 rounded-md shadow-md foreground">
-    <h1 class="text-4xl"> Welcome, AstroTribe is in closed alpha </h1>
-    <p>Currently we have implemented users/profiles, venues and basic event functionalities</p>
-    <p>We will be implementing further functionality every month</p>
-    <p class="font-semibold">
-      If you're interested in joining the waitlist for the beta please
-      <NuxtLink
-        to="auth/register-interest"
-        class="link"
-      >
-        register
-      </NuxtLink>
-    </p>
+  <div class="grid w-full grid-cols-2">
+    <div class="p-8 space-y-1 rounded-md shadow-md foreground">
+      <h1 class="pb-4 text-4xl font-semibold"> Welcome 👋</h1>
+      <h3 class="pb-4 text-xl font-semibold">AstroTribe Is Currently In Closed Alpha</h3>
+      <p>This means we have limited functionality, navigate around to preview the application</p>
+      <p>The good news is that we will be rolling out new features every month!</p>
+      <p class="font-semibold">
+        Interested in joining the beta waitlist?
+        <NuxtLink
+          to="auth/register-interest"
+          class="link"
+        >
+          register your interest here.
+        </NuxtLink>
+      </p>
+    </div>
   </div>
 </template>
 
