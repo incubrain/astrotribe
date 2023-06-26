f5ea9dab6ff885995d3c51aa2e0813ad8e9732fc
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-06-26 22:26:51 +0530

    feat(nav): mobile slideover, nav refactor

--- components/nav/NavMobiBottom.vue
+++ components/nav/NavMobiBottom.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="sticky bottom-0 left-0 flex-row w-full items-center justify-between foreground p-2 h-[var(--nav-height-mobi)]"
+    class="fixed bottom-0 left-0 flex-row w-full items-center justify-between foreground p-2 h-[var(--nav-height-mobi)] border-t border-color"
   >
     <ul class="flex flex-row items-center justify-center w-full h-full">
       <li
@@ -8,7 +8,7 @@
         :key="page.id"
         class="flex items-center justify-center w-full h-full cursor-pointer"
       >
-        <NuxtLink :to="page.slug">
+        <NuxtLink :to="page.slug" class="flex justify-center items-center">
           <UIcon
             :name="page.icon"
             class="w-[20px] h-[20px]"
---
+++ components/nav/NavMobiSlideover.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <UButton
+      variant="link"
+      :padded="false"
+      @click="isOpen = true"
+    >
+      <UAvatar
+        src="/astronera-logo.jpg"
+        alt="Astron Era Logo"
+        class="w-8 h-8 md:w-12 md:h-12"
+      />
+    </UButton>
+    <USlideover
+      v-model="isOpen"
+      side="left"
+    >
+      <div>
+        <div class="foreground w-full flex justify-center items-center p-8">
+          <UButton
+            variant="solid"
+            icon="i-mdi-chevron-left"
+            @click="isOpen = false"
+          >
+            Return
+          </UButton>
+        </div>
+        <NuxtLink
+          v-for="link in links"
+          :key="link.id"
+          :to="link.slug"
+          class="w-full flex justify-start border-b border-color p-4"
+          @click="isOpen = false"
+        >
+          {{ link.name }}
+        </NuxtLink>
+      </div>
+    </USlideover>
+  </div>
+</template>
+
+<script setup lang="ts">
+const isOpen = ref(false)
+
+defineProps({
+  links: {
+    type: Array as PropType<Object[]>,
+    required: true
+  }
+})
+</script>
+
+<style scoped></style>
