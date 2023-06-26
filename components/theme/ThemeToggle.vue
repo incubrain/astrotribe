30d74c705f20dac18775d405bfdfa5c28b90c254
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-06-26 22:26:28 +0530

    ui(fix): small improvements

--- components/theme/ThemeToggle.vue
+++ components/theme/ThemeToggle.vue
@@ -1,7 +1,7 @@
 <template>
   <UIcon
     :name="color.value === 'dark' ? 'i-heroicons-moon' : 'i-heroicons-sun'"
-    class="w-6 h-6 cursor-pointer"
+    class="w-5 h-5 md:w-6 md:h-6 cursor-pointer"
     @click="toggleDark"
   />
 </template>
--- pages/astrotribe/index.vue
+++ pages/astrotribe/index.vue
@@ -1,16 +1,15 @@
 <template>
   <div class="grid w-full grid-cols-1 gap-8 lg:grid-cols-2">
-    <div class="p-8 space-y-1 rounded-md shadow-md foreground">
+    <div class="p-8 rounded-md shadow-md foreground">
       <h1 class="pb-4 text-4xl font-semibold"> Welcome 👋</h1>
       <h3 class="pb-4 text-xl font-semibold">AstroTribe Is Currently In Closed Alpha</h3>
       <p>
         This means we have limited functionality, navigate around to preview the application The
         good news is that we will be rolling out new features every month!
       </p>
-      <p class="font-semibold"> Interested in joining the beta waitlist? </p>
+      <p class="font-semibold mb-6"> Interested in joining the beta waitlist? </p>
       <NuxtLink
         to="auth/register-interest"
-        class="link"
       >
         <UButton color="primary"> Register your interest here. </UButton>
       </NuxtLink>
@@ -18,7 +17,6 @@
   </div>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
