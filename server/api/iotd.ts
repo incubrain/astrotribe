c211389784560aeabb4d19c602dad74e5c4ba938
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-06-28 13:11:44 +0530

    feat(storage): prod/vercelKV, dev/fs for KV store

--- server/api/iotd.ts
+++ server/api/iotd.ts
@@ -13,29 +13,32 @@ const imageObject = z.object({
 
 type NasaImg = z.infer<typeof imageObject>
 
-export default cachedEventHandler(
-  async (event) => {
-    const date = new Date().toISOString().split('T')[0]
-    const cacheKey = `image:${date}`
+export default defineEventHandler(async (event) => {
+  // Create key for KV storage
+  let nasaImg: NasaImg | null
+  const date = new Date().toISOString().split('T')[0]
+  const cacheKey = `nasa-iotd:${date}`
 
-    let nasaImg = await useStorage().getItem(cacheKey)
-    console.log('nasaImg', nasaImg)
-    if (!nasaImg) {
-      const unvalidated = await $fetch(
-        'https://api.nasa.gov/planetary/apod?api_key=qVu1erjdjYJLfLLALZyIz3EfYxOerf29waltn3PM'
-      )
-      const data: NasaImg = imageObject.parse(unvalidated)
-      nasaImg = data
-      // Save the image data to the cache
-      console.log('nasaImg2', nasaImg)
-      await useStorage().setItem(cacheKey, nasaImg)
-    }
+  // Initiate storage redis in prod, fs in dev
+  const storage = useStorage('data')
 
-    console.log('nasaImg3', nasaImg)
-    return nasaImg
-  },
-  {
-    swr: true,
-    maxAge: 60 * 60
+  // Check if item exists in KV storage
+  const isItemInStorage = await storage.hasItem(cacheKey)
+  if (isItemInStorage) {
+    // Get item from KV storage
+    nasaImg = await storage.getItem<NasaImg>(cacheKey)!
+  } else {
+    // Fetch data
+    const nasaKey = useRuntimeConfig().NASA_API_KEY
+    const unvalidated = await $fetch(`https://api.nasa.gov/planetary/apod?api_key=${nasaKey}`)
+
+    // Validate with zod
+    const data: NasaImg = imageObject.parse(unvalidated)
+    nasaImg = data
+
+    // Store in KV
+    await storage.setItem(cacheKey, nasaImg)
   }
-)
+
+  return nasaImg
+})
