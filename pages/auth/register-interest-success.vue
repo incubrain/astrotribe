ee52621ed6b1df1ba4905aaca9cb34dc10ab228d
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-06-29 18:40:22 +0530

    fix(ui): redirect to success on register-interest

---
+++ pages/auth/register-interest-success.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex flex-col items-center justify-center w-full h-full px-4">
+    <h2 class="mb-6 text-2xl text-center">
+      Thanks for your interest!
+    </h2>
+    <h4 class="mb-6 text-center">
+      We will notify you via email once we launch our public beta.
+    </h4>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+definePageMeta({
+  name: 'RegisterInterestSuccess',
+  layout: 'auth'
+})
+</script>
