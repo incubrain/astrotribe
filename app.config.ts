8d069d8703441b325bb5f6c7cf461ad203e2fd4b
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-07-04 18:27:12 +0530

    feat(feed): added initial code for openai summary

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,15 @@
 // This supports HMR
 export default defineAppConfig({
-  title: "AstronEra - AstroTribe",
+  title: 'AstronEra - AstroTribe',
   ui: {
     primary: 'cyan',
+    tooltip: {
+      background: 'background',
+      base: 'invisible lg:visible h-auto px-3 py-2 text-sm font-normal truncat'
+    },
+    modal: {
+      width: 'max-w-[90%] md:max-w-[80%] lg:max-w-[80%] w-auto xl:max-w-[80%]',
+      container: 'flex min-h-full items-center justify-center text-center'
+    }
   }
 })
