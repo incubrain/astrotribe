f2e6428876fe931c5fc2a2a327789d8d2bfa8320
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-07-18 10:36:15 +0530

    chore(package): @sentry/tracing depreciated

--- plugins/sentry.server.ts
+++ plugins/sentry.server.ts
@@ -1,12 +1,13 @@
-import { BrowserTracing } from '@sentry/browser'
 import * as Sentry from '@sentry/browser'
 
 export default defineNuxtPlugin(() => {
   Sentry.init({
     dsn: 'https://eec364410b024a5a837f60e00d367513@o1175094.ingest.sentry.io/4504389151621120',
-    integrations: [new BrowserTracing({
-      tracingOrigins: ['localhost', /^\//]
-    })],
+    integrations: [
+      new Sentry.BrowserTracing({
+        tracingOrigins: ['localhost', /^\//]
+      })
+    ],
     // release,
     // environment,
     // sampleRate: 1,
