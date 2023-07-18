b658fb8a5f3da517027069c619604b4eb8185ea2
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-07-18 21:56:05 +0530

    chore(refactor): users/many api endpoint

--- pages/astrotribe/users/index.vue
+++ pages/astrotribe/users/index.vue
@@ -1,29 +1,24 @@
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:gap-4 xl:gap-8">
-    <CardUser
-      v-for="user in s.users"
-      :key="user.id"
-      :user="user"
-    />
-    <!-- <div class="md:col-span-2">
-            <button
-                class="px-4 py-2 text-white bg-blue-500 rounded-md"
-                @click="post.users.many()"
-            >
-            Create Users</button>
-        </div> -->
+  <div>
+    <div v-if="users !== null" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:gap-4 xl:gap-8">
+      <CardUser
+        v-for="user in users"
+        :key="user.id"
+        :user="user"
+      />
+    </div>
+    <div v-else>
+      No users found
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 definePageMeta({ name: 'All' })
 
-// const post = useCreate()
+const users = ref(null)
 
-const u = useUsersStore()
-await u.testUserRoles({ userId: 1 })
-await u.getUsers({ userId: 1 })
-// await u.getUserFollowed({ userId: 1 })
-
-const s = appState()
+const { error, data } = await useFetch('/api/users/many')
+if (error.value) throw createError(`error getting users: ${error.value.message}`)
+if (data.value) users.value = data.value.users
 </script>
