9fd440678b9040d0b97adec1ec8cf6b894be5c23
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-07-23 11:59:10 +0530

    fix(ui) small requested changes

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
-    <ImageHero :img="about.images[0]" title="The AstronEra Journey" object-position="object-top" />
+    <ImageHero
+      :img="about.images[0]"
+      title="The AstronEra Journey"
+      object-position="object-top"
+    />
 
     <div class="flex flex-col max-w-6xl gap-12 p-6 mx-auto lg:gap-24 lg:py-24">
       <h2 class="text-3xl font-bold leading-7 text-center lg:text-4xl lg:leading-9 pt-6 lg:pt-0">
         Our Story
       </h2>
-
       <div class="grid items-start grid-cols-1 gap-8 lg:grid-cols-2">
+        <div class="w-full leading-6 text-left xl:w-10/12">
+          <h3 class="pb-4 text-2xl font-bold leading-7"> The Mission </h3>
+          <p class="pb-3 text-base font-normal">
+            {{ about.mission }}
+          </p>
+        </div>
+        <div class="w-full leading-6 text-left xl:w-10/12">
+          <h3 class="pb-4 text-2xl font-bold leading-7"> The Vision </h3>
+          <p class="pb-3 text-base font-normal">
+            {{ about.vision }}
+          </p>
+        </div>
         <div class="flex flex-col gap-8">
           <div
             v-for="section in about.sections"
