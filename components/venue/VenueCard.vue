58fc42f1ee6e154ec77bde7cf5db283a6fd02702
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-07-24 14:35:47 +0530

    fix(ui): swipe in animation for cards

--- components/venue/VenueCard.vue
+++ components/venue/VenueCard.vue
@@ -10,7 +10,10 @@
       height="250"
       class="object-contain w-full aspect-square"
     />
-    <div class="absolute w-12 h-12 p-1 overflow-hidden rounded-full left-2 top-2" :class="getBG(venue.id)">
+    <div
+      class="absolute w-12 h-12 p-1 overflow-hidden rounded-full left-2 top-2"
+      :class="getBG(venue.id)"
+    >
       <NuxtImg
         :src="venue.logo"
         loading="lazy"
@@ -20,13 +23,14 @@
         quality="80"
         class="object-contain w-full aspect-square"
       />
-      <div class="flex flex-col gap-2 p-4 foreground md:h-full">
-        <div class="flex justify-between">
-          <h3 class="text-base font-semibold">
-            {{ venue.name?.substring(0, 30) }}
-            {{ venue.name && venue.name.length > 30 ? '...' : '' }}
-          </h3>
-          <!-- <span
+    </div>
+    <div class="flex flex-col gap-2 p-4 foreground md:h-full justify-between">
+      <div class="flex flex-col">
+        <h3 class="text-base font-semibold">
+          {{ venue.name?.substring(0, 30) }}
+          {{ venue.name && venue.name.length > 30 ? '...' : '' }}
+        </h3>
+        <!-- <span
             v-if="venue.avg_rating"
             class="flex items-start gap-1"
           >
@@ -36,7 +40,6 @@
             />
             <p class="text-xs font-semibold"> {{ venue.avg_rating.toPrecision(2) }} </p>
           </span> -->
-        </div>
         <div class="flex flex-col gap-2 text-sm text-gray-500">
           <p>{{ venue.location.state }}, {{ venue.location.country }}</p>
           <p>{{ venue.body }}</p>
@@ -45,6 +48,7 @@
       <a
         :href="venue.website"
         target="_blank"
+        class="mt-4"
       >
         <UButton
           variant="outline"
