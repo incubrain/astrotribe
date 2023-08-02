92ee05a375afc1f808285eafc838e7087c3d26aa
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-08-02 10:29:24 +0530

    feat(api): isAdmin middleware for admin routes

---
+++ server/middleware/1.isAdmin.ts
@@ -0,0 +1,10 @@
+export default defineEventHandler((event) => {
+  const isAdmin = false
+  if (event.path.includes('admin') && !isAdmin) {
+    console.log(`You are not authentcated to use ${event.path}`)
+    return {
+      status: 403,
+      statusText: `You are not authorized to access ${event.path}`
+    }
+  }
+})
