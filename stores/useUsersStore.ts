42430e6606894e514b0869eace6ccde00eeb4578
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-08-10 19:20:44 +0530

    fix(user): checkWeHaveUsers returns boolean

--- stores/useUsersStore.ts
+++ stores/useUsersStore.ts
@@ -4,12 +4,13 @@ export const useUsersStore = defineStore('users', () => {
   const users = ref([] as UserFull[])
   const userCurrent = ref({} as UserFull)
 
-  async function checkWeHaveUsers() {
-    if (users.value.length) return
+  async function checkWeHaveUsers(): Promise<boolean> {
+    if (users.value.length) return true
 
     const { error, data } = await useFetch('/api/users/many')
     if (error.value) throw createError(`error getting users: ${error.value.message}`)
     if (data.value?.users) users.value = data.value.users // todo check data validity
+    return true
   }
 
   async function checkWeHaveUser(id: string) {
