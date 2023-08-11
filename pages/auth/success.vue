08cc60f95ff51352176242e477089d6f6b698388
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-08-11 21:29:52 +0530

    ui(clean): small ui improvements

--- pages/auth/success.vue
+++ pages/auth/success.vue
@@ -2,33 +2,28 @@
   <div class="py-32">
     <h1 class="text-xl font-semibold">Please Confirm Your Email:</h1>
     <div>
-      <h1 class="mb-4">Check your email</h1>
-      <p class="mb-2">
+      <p>
         We’ve sent a message to
         <strong>{{ user?.email }}</strong>
         with a link to activate your account.
       </p>
-      <div class="flex">
-        <div>
-          <p>
-            <a
-              target="_blank"
-              href="https://mail.google.com/mail/u/0/"
-              rel="noopener noreferrer"
-              >Open Gmail</a
-            >
-          </p>
-        </div>
-        <div>
-          <p>
-            <a
-              target="_blank"
-              href="https://outlook.live.com/mail/0/inbox"
-              rel="noopener noreferrer"
-              >Open Outlook</a
-            >
-          </p>
-        </div>
+      <div class="space-x-2 py-4 w-auto">
+        <UBadge>
+          <a
+            target="_blank"
+            href="https://mail.google.com/mail/u/0/"
+            rel="noopener noreferrer"
+            >Open Gmail</a
+          >
+        </UBadge>
+        <UBadge>
+          <a
+            target="_blank"
+            href="https://outlook.live.com/mail/0/inbox"
+            rel="noopener noreferrer"
+            >Open Outlook</a
+          >
+        </UBadge>
       </div>
       <div></div>
       <label>Didn’t get an email? Check your spam folder!</label>
@@ -41,8 +36,13 @@
 </template>
 
 <script setup lang="ts">
-const auth = useAuthStore();
-const { user } = storeToRefs(auth);
+const auth = useAuthStore()
+const { user } = storeToRefs(auth)
+
+definePageMeta({
+  name: 'Success',
+  layout: 'auth'
+})
 </script>
 
 <style scoped></style>
