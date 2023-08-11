2a3a8025b923efbe304d23914e897529f07d8139
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-08-11 17:54:15 +0530

    feat(smtp): add resend for testing

---
+++ composables/useSMTP.ts
@@ -0,0 +1,57 @@
+import { SMTPMessage, SMTPMessages, MessageRequest, MessageRequestSchema } from '@/types/smtp'
+
+export default function useAdmin() {
+  const smtpMessages = {
+    auth: [
+      {
+        type: 'verify-email',
+        subject: 'Welcome to AstronEra! Please Confirm Your Email Address',
+        html: '<strong>It works!</strong>'
+      }
+    ]
+  } as SMTPMessages
+
+  const sendEmail = async (request: MessageRequest) => {
+    const validatedRequest = MessageRequestSchema.safeParse(request)
+    if (!validatedRequest.success) {
+      throw createError(`Error With Input For sendEmail: ${validatedRequest.error}`)
+    }
+
+    const { email, logic, type } = validatedRequest.data
+
+    const messages = smtpMessages[logic]
+    if (!messages) {
+      throw new Error('Invalid logic value')
+    }
+
+    const message = messages.find((message) => message.type === type)
+
+    if (!message) {
+      throw new Error('Message type not found')
+    }
+
+    const { data, error } = await useFetch('/api/smtp/send', {
+      method: 'POST',
+      body: JSON.stringify({ message: { ...message, email } as SMTPMessage })
+    })
+
+    if (error.value) {
+      throw createError({
+        statusCode: 401,
+        message: error.value.message
+      })
+    }
+
+    console.log('email sent data', data.value)
+
+    // const validatedUser = emailUnvalidatedUserSchema.safeParse(data.user)
+
+    // if (!validatedUser.success) {
+    //   throw createError(validatedUser.error)
+    // }
+  }
+
+  return {
+    sendEmail
+  }
+}
