b14b159267d30356372cc30af5d05e51ac220981
AAYUSH TALREJA <2020.aayush.talreja@ves.ac.in>
2023-08-17 11:56:51 +0530

    (fix): gave a try to fix the AuthAPIError and login creds error, which was solved updating the registered email id with a fresh email id; performed tests on various scenarios.

--- stores/useTestingStore.ts
+++ stores/useTestingStore.ts
@@ -13,7 +13,15 @@ export default defineStore('admin-settings', () => {
   const authStore = useAuthStore()
   const user = { email: env.TESTING_USERNAME, password: env.TESTING_PASSWORD }
   const auth = {
-    login: async () => await authStore.login(user),
+    login: async () => {
+      try {
+        const result = await authStore.login(user)
+        console.log('Login Successful:', result)
+      } catch (error) {
+        console.error('(test) Login Error:', error)
+        throw error
+      }
+    },
     register: async () => await authStore.register(user),
     componentVisible: () => !settings.authOn || authStore.hasSession
   }
