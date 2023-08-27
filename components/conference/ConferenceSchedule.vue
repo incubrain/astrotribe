fe3204de58ad367539030e22f507a865401c35cb
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-08-27 10:26:41 +0530

    ui(conference): small improvements

--- components/conference/ConferenceSchedule.vue
+++ components/conference/ConferenceSchedule.vue
@@ -4,12 +4,21 @@
       :title="schedule.title"
       :subtitle="schedule.subtitle"
     />
+    <ConferenceTitle
+      :title="speakers.title"
+      :subtitle="speakers.subtitle"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 const schedule = {
   title: 'Conference Schedule',
+  subtitle: 'Coming soon!'
+}
+
+const speakers = {
+  title: 'Speaker List',
   subtitle: 'Coming soon!'
 }
 </script>
