dd1feb27e5be9f06993314ed3bc1bc2541711b99
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-11-20 11:23:23 +0530

    fix(ui): mobi bottom nav overlapping content

--- layouts/app.vue
+++ layouts/app.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="w-full h-full flex min-h-[calc(100vh-var(--nav-height-sm))] lg:min-h-[calc(100vh-var(--nav-height-lg))]">
-    <NavSidebar
-      class="w-full min-h-full max-w-[10%] min-w-[240px]"
-    />
+  <div class="w-full h-full flex">
+    <NavSidebar class="w-full min-h-full max-w-[10%] min-w-[240px]" />
 
-    <div class="w-full mx-auto md:p-4 xl:p-8">
+    <div
+      class="w-full mx-auto md:p-4 xl:p-8 min-h-[calc(100vh-var(--nav-height-sm))] lg:min-h-[calc(100vh-var(--nav-height-lg))] pb-[var(--mobi-bottom-nav-height)]"
+    >
       <slot />
     </div>
     <NavMobiBottom class="lg:hidden" />
