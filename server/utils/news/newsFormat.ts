64b872d2b3161e776874c0627c0aa8b4fdafa526
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-11-21 11:24:44 +0530

    chore: add comments to scraping / openAI code

--- server/utils/news/newsFormat.ts
+++ server/utils/news/newsFormat.ts
@@ -1,50 +1,62 @@
 import type { NewsType, NewsScrapedType } from '@/types/news'
 import type { AuthorType } from '@/types/authors'
 
+// Function to format images and videos into a standardized media format.
 const formatMedia = (images: any, videos: any) => {
+  // Combine images and videos into one array, handling null or undefined inputs.
   const media = [...(images || []), ...(videos || [])]
+
+  // Return null if there are no media items to format.
   if (media.length === 0) return null
 
+  // Transform each media item to a standardized format.
   return media.map((mediaItem: { src: string; alt: string; caption: string; credit: string }) => ({
-    src: mediaItem.src,
-    alt: mediaItem.alt || null,
-    caption: mediaItem.caption || null,
-    credit: mediaItem.credit || null
+    src: mediaItem.src, // Source URL of the media.
+    alt: mediaItem.alt || null, // Alternate text, null if not provided.
+    caption: mediaItem.caption || null, // Caption for the media, null if not provided.
+    credit: mediaItem.credit || null // Credit for the media, null if not provided.
   }))
 }
 
-// The goal of this function is to take the scraped data and format it to match the News database table
+/// Function to format scraped data to match the structure of the News database table.
 const newsFormat = (
   scrapedData: NewsScrapedType[]
 ): {
   newsPost: Partial<NewsType>[]
   authors: Partial<AuthorType>[]
 } => {
+  // Initialize arrays to store formatted news posts and authors.
   const newsPosts: Partial<NewsType>[] = []
   const authors: Partial<AuthorType>[] = []
 
+  // Iterate over each piece of scraped data.
   for (const data of scrapedData) {
+    // Format each news post.
     const newsPost: Partial<NewsType> = {
-      created_at: data.created_at,
-      updated_at: data.updated_at || data.created_at,
-      title: data.title,
-      link: data.link,
-      body: data.body || undefined,
-      media: formatMedia(data.images, data.videos),
-      category_id: 31 // or determine category from the scraped data
+      created_at: data.created_at, // Creation date of the post.
+      updated_at: data.updated_at || data.created_at, // Update date, fallback to created_at if not available.
+      title: data.title, // Title of the news post.
+      link: data.link, // Link to the original news post.
+      body: data.body || undefined, // Body of the news post, undefined if not present.
+      media: formatMedia(data.images, data.videos), // Formatted media items.
+      category_id: 31 // Default category ID, can be dynamically determined.
     }
+    // Add the formatted news post to the array.
     newsPosts.push(newsPost)
 
+    // Process the author if available.
     if (data.author) {
       const author: Partial<AuthorType> = {
-        name: data.author.name || null,
-        link: null,
-        image: data.author.image || null
+        name: data.author.name || null, // Name of the author, null if not available.
+        link: null, // Link to the author's profile, null here.
+        image: data.author.image || null // Image of the author, null if not available.
       }
+      // Add the formatted author to the array.
       authors.push(author)
     }
   }
 
+  // Return the formatted news posts and authors.
   return { newsPost: newsPosts, authors }
 }
 
--- server/utils/news/newsScraperBase.ts
+++ server/utils/news/newsScraperBase.ts
@@ -6,44 +6,66 @@ interface ScrapeFunction {
   (browser: Browser, blog: Blog): Promise<any[]>
 }
 
+// Define a base scraping function for news.
 const newsScraperBase: ScrapeFunction = async (browser: Browser, blog: Blog) => {
+  // Log the start of scraping for a specific blog.
   console.log(`newsScraperBase: scrape ${blog.name}`)
-  const maxArticles = 10 // You can adjust this value
+
+  // Set a maximum number of articles to scrape, useful for testing.
+  const maxArticles = 10
   let posts: any[] = []
 
+  // Open a new browser page.
   const page: Page = await browser.newPage()
+  // Navigate to the blog's URL.
   await page.goto(blog.url)
 
+  // Loop indefinitely until the break condition is met.
   while (true) {
     try {
+      // Use the blog's specific scraping function to extract data.
       const data: any[] = await blog.scraper(page, blog)
+      // Add the newly scraped data to our posts array.
       posts = [...posts, ...data]
 
+      // Check if the number of posts meets or exceeds the maxArticles limit.
       if (maxArticles && posts.length >= maxArticles) {
+        // Log the achievement of max articles and break the loop.
         console.log(`newsScraperBase: reached max articles ${posts.length}`)
         break
       }
 
+      // Find the link to the next page of the blog.
       const nextPageLink = await page.$$eval(blog.selectorPagination, (elements) =>
         elements.length ? elements[0].getAttribute('href') : null
       )
 
+      // If there is no link to a next page, log and break the loop.
       if (!nextPageLink) {
         console.log(`newsScraperBase: last page ${posts.length}`)
         break
       }
 
+      // Log moving to the next page.
       console.log('newsScraperBase: next page')
+      // Wait for 2 seconds before proceeding.
       await new Promise((resolve) => setTimeout(resolve, 2000))
+      // Go to the next page.
       await page.goto(nextPageLink)
     } catch (error: any) {
+      // Log any errors encountered during scraping.
       console.error(`newsScraperBase: error scraping page - ${error.message}`, error.stack)
+      // Break the loop if an error occurs.
       break
     }
   }
-  console.log('newsScraperBase: try formatting posts')
-  const formattedPosts = newsFormat(posts) // Assuming newsFormat is defined elsewhere
 
+  // Log the attempt to format the scraped posts.
+  console.log('newsScraperBase: try formatting posts')
+  // Format the posts using a predefined formatting function.
+  const formattedPosts = newsFormat(posts)
+
+  // Return the formatted posts, cast to the NewsType type.
   return formattedPosts as NewsType[]
 }
 
