738db0debd416905de0f3b92b358561b8ff3a361
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-03-16 13:17:26 +0530

    app grid improvement

--- pages/astrotribe/users/index.vue
+++ pages/astrotribe/users/index.vue
@@ -1,8 +1,18 @@
+<script setup lang="ts">
+const userStore = useUsersStore()
+const haveUsers = await userStore.checkWeHaveUsers()
+const { users } = storeToRefs(userStore)
+
+console.log('users', users)
+
+definePageMeta({ name: 'Users', layout: 'app' })
+</script>
+
 <template>
   <div>
     <div
       v-if="haveUsers"
-      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:gap-4 xl:gap-8"
+      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 md:gap-4 xl:gap-8"
     >
       <LazyUserCard
         v-for="user in users"
@@ -12,11 +22,3 @@
     </div>
   </div>
 </template>
-
-<script setup lang="ts">
-const u = useUsersStore()
-const haveUsers = await u.checkWeHaveUsers()
-const { users } = storeToRefs(u)
-
-definePageMeta({ name: 'Users', layout: 'app' })
-</script>
