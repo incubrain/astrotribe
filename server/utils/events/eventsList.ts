ad96712ce78cbfeabfdbbd27e6dfc6675755b012
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-03-20 11:43:10 +0530

    chore: add lists of places to scrape

---
+++ server/utils/events/eventsList.ts
@@ -0,0 +1,8 @@
+// https://www.stsci.edu/jwst/news-events/events
+// https://nightsky.jpl.nasa.gov/event-list.cfm
+// https://solarsystem.nasa.gov/solar-system-ambassadors/events/
+// https://skyandtelescope.org/astronomy-events/
+
+// upcoming launches:
+// https://www.nasa.gov/events/
+// https://www.isro.gov.in/FutureMissions.html
