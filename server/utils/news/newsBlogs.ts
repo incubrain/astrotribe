a7a3917ee08104231b1b1683cf036183305931ce
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-03-20 11:43:10 +0530

    chore: add lists of places to scrape

--- server/utils/news/newsBlogs.ts
+++ server/utils/news/newsBlogs.ts
@@ -19,13 +19,47 @@ export interface Blog {
   selectorConfig: SelectorConfig
 }
 
+// consider:
+// 1
+// https://www.stsci.edu/jwst/news-events/news
+// https://webbtelescope.org/news/news-releases
+// https://www.stsci.edu/news/newsletters
+// https://hubblesite.org/news/news-releases
+// https://www.nasa.gov/news/all-news/ // latest news from nasa
+// https://www.jpl.nasa.gov/news
+
+// https://earthobservatory.nasa.gov/blogs/ // multiple blogs
+
+// https://nightsky.jpl.nasa.gov/news-archive.cfm
+
 // Array containing configurations for different blogs to be scraped.
 const newsBlogs: Blog[] = [
   {
-    // Configuration for the first blog.
+    // Nasa Config
     id: 1,
     name: 'jwst-nasa-blog', // Name of the blog.
-    url: 'https://blogs.nasa.gov/webb/', // URL of the blog.
+    url: [
+      'https://blogs.nasa.gov/webb/',
+      'https://blogs.nasa.gov/spacestation/',
+      'https://blogs.nasa.gov/commercialcrew/',
+      'https://blogs.nasa.gov/artemis/',
+      'https://blogs.nasa.gov/clps/',
+      'https://blogs.nasa.gov/earthexpeditions/',
+      'https://blogs.nasa.gov/nelson/',
+      'https://blogs.nasa.gov/ixpe/',
+      'https://blogs.nasa.gov/interns/',
+      'https://blogs.nasa.gov/stationreport/', // maybe not?
+      'https://blogs.nasa.gov/kennedy/',
+      'https://blogs.nasa.gov/northropgrumman/',
+      'https://blogs.nasa.gov/osiris-rex/',
+      'https://blogs.nasa.gov/parkersolarprobe/',
+      'https://blogs.nasa.gov/sofia/',
+      'https://blogs.nasa.gov/spacex/',
+      'https://blogs.nasa.gov/Watch_the_Skies/',
+      'https://blogs.nasa.gov/sunspot/',
+      'https://blogs.nasa.gov/odeo/',
+      ''
+    ],
     selectorBase: 'article', // Base selector for scraping.
     selectorPagination: '.nav-links .next', // Selector for the next page link.
     scraper: newsScraperNasa, // Function used to scrape this specific blog.
