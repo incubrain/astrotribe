c808766dba5cc4ebb77782f3f2ec009a8874d7ca
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-03-24 14:36:26 +0530

    research different data to scrape

---
+++ server/utils/organizations/organizationsList.ts
@@ -0,0 +1,130 @@
+// https://www.astro4dev.org/projects-search/
+// https://jwst.nasa.gov/content/meetTheTeam/team.html#fullTextListingOfContributors
+// https://www.iau.org/administration/membership/national/
+// https://skyandtelescope.org/astronomy-club-organization-directory/clubs_category/clubs/
+// https://skyandtelescope.org/astronomy-club-organization-directory/clubs_category/special-interest/
+// https://en.wikipedia.org/wiki/List_of_private_spaceflight_companies
+// https://esoc.esa.int/supporting-zero-debris-charter
+
+// companies
+// https://www.isro.gov.in/spacetutor.html
+// https://www.isro.gov.in/registered-isro-merchandisers-01112021.html
+// https://www.isro.gov.in/media_isro/pdf/programme/List_of_Ongoing_RESPOND_Projects_as_of_July_2023.pdf
+// https://www.isro.gov.in/media_isro/pdf/programme/RESPOND_COORDINATORS_OF_ISRO.pdf
+// https://en.wikipedia.org/wiki/Space_industry_of_India
+// https://en.wikipedia.org/wiki/Indian_Space_Association
+
+// Gov
+// https://www.antrix.co.in/
+// https://www.sac.gov.in/Vyom/
+// https://www.vssc.gov.in/
+// https://ispa.space/
+// https://hal-india.co.in/home
+// https://www.nsilindia.co.in/
+
+// Private
+// https://space-india.com/
+// https://axsx.in/
+// https://skyroot.in/index.html
+// https://agnikul.in/#/
+// https://www.pixxel.space/
+// https://www.nsilindia.co.in/
+// https://bellatrix.aero/
+// https://www.dhruvaspace.com/
+// https://www.aeroin.space/
+// https://aadyah.com/
+// https://astrogatelabs.com/
+// https://blueskyhq.io/
+// https://www.digantara.co.in/
+// https://www.eonspacelabs.com/
+// https://www.galaxeye.space/
+// https://www.teamindus.in/
+// https://satellize.com/
+// https://www.satsure.co/
+// https://www.axiomspace.com/
+// Me
+// https://www.starlabsurat.com/
+// https://spacedevelopmentnexus.com/
+// https://www.parasdefence.com/
+// https://spaceaura.com/
+// https://www.spaceroboticssociety.org/
+// https://spacekidzindia.in/
+// https://piersight.space/
+// https://vellon.in/
+// https://www.nss-mumbai.org/
+// https://www.numer8.in/
+// https://deltavanalytics.com/
+// https://originantares.in/
+// https://ananthtech.com/
+// https://www.datapatternsindia.com/
+// https://www.godrej.com/aerospace-and-defence
+// https://nspacetech.in/
+// https://www.manastuspace.com/
+// https://spacim.co.in/
+// https://www.digantara.co.in/
+// http://www.spacefields.in/
+// https://www.spaceskool.com/
+// https://astramwp.com/
+// https://www.azistabst.com/
+// https://www.orbitx.in/
+// https://www.korrai.com/
+// https://skyserve.ai/
+// https://www.etherealx.space/
+// https://abyom.com/
+// https://hexstaruniverse.com/
+// https://www.dhruvaspace.com/
+// https://www.spacemachines.co/
+// https://suhora.com/#/
+// https://dronvayu.com/
+// https://www.kawa.space/
+
+// Linkedin Categories
+// Space Research and Technology
+// Aviation and Aerospace Component Manufacturing
+// Defense and Space Manufacturing
+// Aviation & Aerospace
+
+// groups & non-profits
+// https://ciaa1.tripod.com/astrogr.htm
+// https://skyandtelescope.org/astronomy-club-organization-directory/clubs_tag/india/
+// https://www.meetup.com/topics/beginner-astronomy/in/
+// https://bangaloreastronomyclub.com/
+// https://stargazingmumbai.in/
+// https://ritindia.edu/spaceclub/index.html
+// https://spats.co.in/
+// https://skyline-space.com/
+// https://www.agnirva.com/
+// https://nssc.in/
+// https://blackholespacetech.in/
+//
+
+// New Zealand
+// http://www.spaceops.nz/
+// https://emrod.energy/
+// https://www.zenno.space/
+// https://www.dawnaerospace.com/
+// https://zenithtecnica.com/
+// https://swarm.space/
+// https://www.rocketlabusa.com/
+// https://spacebase.co/ // directory
+// https://ruamokoinnovation.com/
+// https://www.planet.com/
+// https://www.flatwavefronts.com/
+// https://www.stardustme.com/
+// https://xerra.nz/
+// https://kiwistaroptics.com/
+// https://geospatial.ac.nz/
+
+
+// Japan
+// https://astroscale.com/
+// https://gitai.tech/
+// https://www.axelspace.com/
+// https://space-bd.com/en/
+// 
+
+// Canada
+// https://mda.space/
+// https://northstar-data.com/
+// https://gdmissionsystems.ca/space
+// https://www.spectrumcontrol.com/aerospace-defense/space
