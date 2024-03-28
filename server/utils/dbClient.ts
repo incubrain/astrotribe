16c7ca81342a76eadf9b1666fb2417326b678f05
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-03-28 11:37:41 +0530

    revert to schema.gen.ts for supabase types

--- server/utils/dbClient.ts
+++ server/utils/dbClient.ts
@@ -1,6 +1,6 @@
 import type { SupabaseClient } from '@supabase/supabase-js'
 import type { H3Event } from 'h3'
-import type { Database } from '~/supabase/database.gen'
+import type { Database } from '~/supabase/schema.gen'
 import { serverSupabaseClient, serverSupabaseServiceRole } from '#supabase/server'
 
 export async function dbClient(event: H3Event): Promise<SupabaseClient> {
