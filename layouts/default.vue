6bba9360ecb88df025f7cfcac639220101202c54
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-04-01 11:17:56 +0530

    remove nav from contact page, add back button

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,21 @@
 <template>
-  <div class="w-full min-h-screen foreground">
-    <NavTop />
+  <div class="w-full min-h-screen foreground relative">
+    <NavTop v-if="showTopNav" />
+    <AppBackButton
+      v-else
+      class="fixed bottom-2 left-2 z-50"
+    />
     <slot />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const route = useRoute()
+const noTopNav = ['/contact']
+
+const showTopNav = computed(
+  () => !noTopNav.some((noTopNavPath) => route.path.startsWith(noTopNavPath))
+)
+</script>
 
 <style></style>
