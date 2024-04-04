4afc5a665ac36969c3734ac5076570b8b9ddee1e
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-04-04 18:55:09 +0530

    change /users to /user api route

--- stores/useUsersStore.ts
+++ stores/useUsersStore.ts
@@ -7,7 +7,7 @@ export const useUsersStore = defineStore('users', () => {
   async function checkWeHaveUsers(): Promise<boolean> {
     if (users.value.length) return true
 
-    const { error, data } = await useFetch('/api/users/many', {
+    const { error, data } = await useFetch('/api/user/many', {
       headers: useRequestHeaders(['cookie'])
     })
     if (error.value) throw createError(`error getting users: ${error.value.message}`)
@@ -19,7 +19,7 @@ export const useUsersStore = defineStore('users', () => {
   async function checkWeHaveUser(id: string) {
     if (userCurrent.value.id && userCurrent.value.id === id) return true
 
-    const { error, data } = await useFetch(`/api/users/${id}`)
+    const { error, data } = await useFetch(`/api/user/${id}`)
     if (error.value) throw createError(`error getting users: ${error.value.message}`)
     if (data.value?.user) {
       userCurrent.value = data.value.user // todo check data validity
