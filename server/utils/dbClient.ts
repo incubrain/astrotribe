0eacf0d70964c4b86f8869eee84f3c05d5430c62
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-04-05 22:25:08 +0530

    fix type intellisense for server dbClient

--- server/utils/dbClient.ts
+++ server/utils/dbClient.ts
@@ -3,10 +3,10 @@ import type { H3Event } from 'h3'
 import type { Database } from '~/supabase/schema.gen'
 import { serverSupabaseClient, serverSupabaseServiceRole } from '#supabase/server'
 
-export async function dbClient(event: H3Event): Promise<SupabaseClient> {
+export async function dbClient(event: H3Event): Promise<SupabaseClient<Database>> {
   return await serverSupabaseClient<Database>(event)
 }
 
-export function dbClientAdmin(event: H3Event): SupabaseClient {
+export function dbClientAdmin(event: H3Event): SupabaseClient<Database> {
   return serverSupabaseServiceRole<Database>(event)
 }
