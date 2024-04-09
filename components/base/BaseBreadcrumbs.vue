529ad82ba0701f1568879e9a4045e70610046ee8
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-04-09 17:40:16 +0530

    create templates for base components

---
+++ components/base/BaseBreadcrumbs.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const links = useBreadcrumbItems()
+</script>
+
+<template>
+  <PrimeBreadcrumb
+    :model="links"
+    :pt="{
+      root: 'foreground'
+    }"
+  />
+</template>
+
+<style></style>
