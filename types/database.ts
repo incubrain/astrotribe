4c54e36ae95ef4ebce45b52eb2f4b969f4c02d50
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-04-15 17:02:24 +0530

    cleanup types

--- types/database.ts
+++ types/database.ts
@@ -1,6 +1,6 @@
 import type { Tables } from '~/supabase/schema.gen'
 
-export type RowUser = Tables<'users'>
+export type RowUser = Tables<'user_profiles'>
 export type RowUserFollowers = Tables<'user_followers'>
 export type RowRole = Tables<'roles'>
 export type RowRegisterInterest = Tables<'register_interest'>
@@ -15,3 +15,18 @@ export type RowEmbedding = Tables<'embeddings'>
 export type RowNewsEmbedding = Tables<'news_embeddings'>
 
 export type RowPaper = Tables<'papers'>
+
+type ArrayToUnion<A> = A extends readonly (infer T)[] ? T : never
+
+const userCardKeys = [
+  'id',
+  'given_name',
+  'surname',
+  'username',
+  'avatar',
+  'followed_count',
+  'followers_count',
+  'role_id'
+] as const
+
+export type UserCard = Pick<RowUser, ArrayToUnion<typeof userCardKeys>>
