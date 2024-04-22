ad5c397f579322be4d4ffcbb268008f4403b97c1
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-04-22 15:12:58 +0530

    ui: add logos to companies, create seed data

--- pages/astrotribe/companies.vue
+++ pages/astrotribe/companies.vue
@@ -1,76 +1,13 @@
 <script setup lang="ts">
+import localCompanies from '@/supabase/seed/companies.json'
 const companiesStore = useCompaniesStore()
 const { companies } = storeToRefs(companiesStore)
 
-const haveCompanies = computed(() => companies.value !== null && companies.value.length > 0)
+const haveCompanies = computed(() => companiesStore !== null && companiesStore.length > 0)
 
 const paginationStore = usePaginationStore()
 // blog_url: 'https://agnikul.in/#/news', // external blogs, just title, link and description
 // jobs_page_url: 'https://agnikul.in/#/career', // annoying dropdowns to select job type and job, no clear date
-
-const firstCompany = {
-  name: 'Agnikul Cosmos',
-  description:
-    'Agnikul Cosmos is a space tech company building small satellite launch vehicles. We are building a full stack launch vehicle to take small satellites to space.',
-  founding_year: 2017,
-  logo_url: 'https://agnikul.in/static/media/logo.4d8e7f0d.png',
-  website_url: 'https://agnikul.in/#/',
-  is_government: false,
-  category_id: 1,
-  last_scraped_at: new Date(),
-  scrape_frequency: 'BiWeekly',
-  contacts: [
-    {
-      contact_type: 'Recruitment',
-      title: 'Srinath Ravichandran',
-      email: 'humancapital@agnikul.in',
-      is_primary: false,
-      privacy_level: 'Private'
-    },
-    {
-      contact_type: 'Company',
-      title: 'Srinath Ravichandran',
-      is_primary: true,
-      email: 'curious@agnikul.in',
-      phone: '+91 72472 46334',
-      privacy_level: 'Public'
-    }
-  ],
-  cities: [
-    {
-      name: 'Thiruvananthapuram',
-      state: 'Kerala',
-      country_id: 1
-    },
-    {
-      name: 'Chennai',
-      state: 'Kerala',
-      country_id: 1
-    }
-  ],
-  addresses: [
-    {
-      name: 'Agnikul Cosmos Private Limited.',
-      street1: 'Rocket Factory, IIT Madras Research Park',
-      street2: '1st Floor, A-Block',
-      address_type: 'Factory',
-      is_primary: false
-    },
-    {
-      name: 'Agnikul Cosmos Launch Vehicles Private Limited',
-      street1: 'Kerala Startup Mission, Technopark',
-      street2: 'Thejaswini, G3B, Technopark Rd, Karyavattom',
-      is_primary: true,
-      address_type: 'Headquarters'
-    }
-  ],
-  social_media: {
-    linkedin_url: 'https://www.linkedin.com/company/agnikul-cosmos/',
-    twitter_url: 'https://twitter.com/@agnikulcosmos',
-    instagram_url: 'https://instagram.com/agnikul/',
-    youtube_url: 'https://www.youtube.com/@agnikulcosmos1404/videos'
-  }
-}
 
 const fetchInput = ref({
   storeKey: 'companiesStore',
@@ -94,7 +31,7 @@ definePageMeta({ name: 'Companies', layout: 'app' })
 <template>
   <div>
     <div class="w-full">
-      <PrimeButton @click="companiesStore.insertCompany(firstCompany)"> Add Company </PrimeButton>
+      <!-- <PrimeButton @click="companiesStore.insertCompany(firstCompany)"> Add Company </PrimeButton> -->
     </div>
     <BaseInfiniteScroll
       store-key="companiesStore"
@@ -104,12 +41,9 @@ definePageMeta({ name: 'Companies', layout: 'app' })
       }"
       @update:scroll-end="companiesStore.loadCompanies(fetchInput)"
     >
-      <div
-        v-if="haveCompanies"
-        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:gap-4 xl:gap-8"
-      >
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:gap-4 xl:gap-8">
         <CompanyCard
-          v-for="(company, i) in companies"
+          v-for="(company, i) in localCompanies"
           :key="`companies-post-${i}`"
           :company="company"
         />
