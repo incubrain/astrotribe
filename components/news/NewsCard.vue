9ff49281240a0ac738d2aae9e163bae84d6522d4
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-04-24 21:42:00 +0530

    ui: vertical news card design, added skeleton loader

--- components/news/NewsCard.vue
+++ components/news/NewsCard.vue
@@ -14,74 +14,80 @@ defineProps({
 </script>
 
 <template>
-  <BaseHoverCard>
-    <div>
-      <!-- <div
-        v-if="news.featured_image"
-        class="overflow-hidden sm:h-48"
-      >
-        <BaseImage
+  <div class="border-b border-color">
+    <div class="p-6 flex gap-4">
+      <BaseImage
+        :img="{
+          src: `images/companies/nasa-logo.jpg`,
+          alt: news.title,
+          width: '60',
+          height: '60'
+        }"
+        class="rounded-full"
+      />
+      <div class="flex flex-col gap-2 justify-center-center">
+        <PrimeTag>
+          {{ news.source.toUpperCase() }}
+        </PrimeTag>
+        <span class="text-sm w-auto">
+          {{ useTimeAgo(news.published_at ?? news.created_at).value }}
+        </span>
+      </div>
+    </div>
+    <div
+      v-if="news.featured_image"
+      class="overflow-hidden w-full"
+    >
+      <BaseImage
         :img="{
           src: news.featured_image,
           alt: news.title,
           fit: 'cover',
-          width: '420',
-          height: '220',
+          width: '220',
+          height: '220'
         }"
-        class="rounded-t-md object-cover object-center w-full h-full aspect-video"
-        />
-      </div> -->
-      <!-- <div
-        v-else
-        class="overflow-hidden sm:h-48"
-      >
-        <BaseImage
+        class="rounded-t-md object-cover object-center w-full h-full"
+      />
+    </div>
+    <div
+      v-else
+      class="overflow-hidden sm:h-48"
+    >
+      <BaseImage
         :img="{
           src: `images/news/${news.source}-placeholder.jpg`,
           alt: news.title,
           fit: 'cover',
-          width: '420',
-          height: '220',
+          width: '220',
+          height: '220'
         }"
         class="rounded-t-md object-cover object-center w-full h-full aspect-video"
-        />
-      </div> -->
-      <div class="space-y-3">
-        <div class="flex gap-2 items-center justify-between">
-          <PrimeTag>
-            {{ news.source.toUpperCase() }}
-          </PrimeTag>
-          <span class="text-sm w-auto">
-            {{ useTimeAgo(news.published_at ?? news.created_at).value }}
-          </span>
-          <NuxtLink
-            :to="news.url"
-            target="_blank"
-            rel="noopener"
-          >
-            <PrimeButton
-              label="Read more"
-              size="small"
-              text
-            />
-          </NuxtLink>
-        </div>
-      </div>
+      />
     </div>
-    <div
-      class="flex flex-col gap-2 pb-4 group-hover:max-h-none group-hover:overflow-scroll overflow-hidden"
-    >
-      <h4
-        class="text-balance text-xl md:max-h-16 group-hover:max-h-none group-hover:overflow-visible overflow-hidden"
-      >
+    <div class="flex flex-col gap-2 p-4">
+      <div class="w-full"> </div>
+      <h4 class="text-balance text-xl">
         {{ news.title }}
       </h4>
       <p
         v-if="news.description"
-        class="text-sm max-h-none md:max-h-4 overflow-hidden lg:group-hover:max-h-none lg:group-hover:overflow-visible"
+        class="text-sm"
       >
         {{ news.description }}
       </p>
+      <div class="w-full flex justify-end p-2">
+        <NuxtLink
+          :to="news.url"
+          target="_blank"
+          rel="noopener"
+        >
+          <PrimeButton
+            label="Read more"
+            size="small"
+            outlined
+          />
+        </NuxtLink>
+      </div>
     </div>
-  </BaseHoverCard>
+  </div>
 </template>
