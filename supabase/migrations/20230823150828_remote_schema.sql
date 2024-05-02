2d5867d003f4919372c34172c7188028e9985436
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-05-02 16:30:28 +0530

    fix(critical): reset migration history, cleanup for production

--- supabase/migrations/20230823150828_remote_schema.sql
+++
@@ -1,7 +0,0 @@
-alter table "public"."_prisma_migrations" drop constraint "_prisma_migrations_pkey";
-
-drop index if exists "public"."_prisma_migrations_pkey";
-
-drop table "public"."_prisma_migrations";
-
-
