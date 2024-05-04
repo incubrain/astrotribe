66a48978fb2f23ff2cc78992a6871d4ccd13943c
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-05-04 08:36:16 +0530

    ui: register page update

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -18,50 +18,74 @@ definePageMeta({
 </script>
 
 <template>
-  <PrimeCard class="flex flex-col items-center justify-center w-full h-full">
-    <template #title>
-      <h2 class="text-2xl text-center"> Register </h2>
-    </template>
+  <AuthCard
+    :title="{
+      main: 'Create an Account',
+      subtitle: 'Already have an account?',
+      label: 'Login here.'
+    }"
+    help-url="/auth/login"
+  >
     <template #content>
-      <div class="flex flex-col gap-4 xl:gap-6">
+      <div class="flex flex-col gap-4">
+        <div class="flex gap-4 w-full">
+          <PrimeFloatLabel class="w-full">
+            <PrimeInputText
+              class="w-full"
+              id="given_name"
+              v-model="form.given_name"
+            />
+            <label for="given_name">Given Name</label>
+          </PrimeFloatLabel>
+          <PrimeFloatLabel class="w-full">
+            <PrimeInputText
+              id="surname"
+              v-model="form.surname"
+              class="w-full"
+            />
+            <label for="surname">Surname</label>
+          </PrimeFloatLabel>
+        </div>
         <PrimeFloatLabel class="flex flex-col w-full">
           <PrimeInputText
-            id="username"
+            id="email"
             v-model="form.email"
           />
-          <label for="username">Username</label>
+          <label for="email">Email</label>
         </PrimeFloatLabel>
-        <PrimeFloatLabel>
-          <PrimePassword
-            id="password"
-            v-model="form.password"
-            class="flex flex-col relative"
-          >
-            <template #footer>
-              <PrimeDivider />
-              <p class="mt-2">Suggestions</p>
-              <ul
-                class="pl-2 ml-2 mt-0"
-                style="line-height: 1.5"
-              >
-                <li>At least one lowercase</li>
-                <li>At least one uppercase</li>
-                <li>At least one numeric</li>
-                <li>Minimum 8 characters</li>
-              </ul>
-            </template>
-          </PrimePassword>
-          <label for="password">Password</label>
-        </PrimeFloatLabel>
-        <PrimeFloatLabel>
-          <PrimePassword
-            id="confirmPassword"
-            v-model="form.confirmPassword"
-            :feedback="false"
-            class="flex flex-col relative"
-          />
-          <label for="confirmPassword">Confirm password</label>
-        </PrimeFloatLabel>
+        <div class="flex gap-4 w-full">
+          <PrimeFloatLabel class="w-full">
+            <PrimePassword
+              id="password"
+              v-model="form.password"
+              class="w-full"
+            >
+              <template #footer>
+                <PrimeDivider />
+                <p class="mt-2">Suggestions</p>
+                <ul
+                  class="pl-2 ml-2 mt-0"
+                  style="line-height: 1.5"
+                >
+                  <li>At least one lowercase</li>
+                  <li>At least one uppercase</li>
+                  <li>At least one numeric</li>
+                  <li>Minimum 8 characters</li>
+                </ul>
+              </template>
+            </PrimePassword>
+            <label for="password">Password</label>
+          </PrimeFloatLabel>
+          <PrimeFloatLabel class="w-full">
+            <PrimePassword
+              id="confirmPassword"
+              v-model="form.confirmPassword"
+              :feedback="false"
+              :pt="{ root: 'w-full', input: 'w-full' }"
+            />
+            <label for="confirmPassword">Confirm password</label>
+          </PrimeFloatLabel>
+        </div>
         <PrimeButton
           class="justify-center"
           :disabled="isPasswordInvalid"
@@ -72,6 +96,14 @@ definePageMeta({
       </div>
     </template>
     <template #footer>
+      <div class="w-full">
+        <PrimeDivider
+          layout="horizontal"
+          class="flex justify-left items-center"
+        >
+          <p>Or continue with</p>
+        </PrimeDivider>
+      </div>
       <div class="flex flex-col md:flex-row gap-4 xl:gap-6 w-full">
         <AuthSocialButton
           provider="twitter"
@@ -82,9 +114,6 @@ definePageMeta({
           @social-login="auth.loginSocial('linkedin_oidc')"
         />
       </div>
-      <p class="mt-4 text-sm text-center">
-        <NuxtLink to="/auth/login"> Already have an account? Sign In </NuxtLink>
-      </p>
     </template>
-  </PrimeCard>
+  </AuthCard>
 </template>
