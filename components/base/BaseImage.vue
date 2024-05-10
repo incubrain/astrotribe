7381d84913fc0a1426327f2747ef244e1dfa5e44
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-05-10 12:08:49 +0530

    fix: fallback image on NuxtImg error

--- components/base/BaseImage.vue
+++ components/base/BaseImage.vue
@@ -1,23 +1,31 @@
 <script lang="ts" setup>
 // const nonce = useNonce()
 
-defineProps({
+const props = defineProps({
   img: {
     type: Object,
     required: true
   }
 })
+
+
+const imageUrl = ref(props.img.src)
+function handleError() {
+  imageUrl.value = 'fallback.jpg'
+}
+
 </script>
 
 <template>
   <NuxtImg
-    :src="img.src ?? 'fallback.jpg'"
+    :src="img.src"
     :alt="img.alt"
     :width="img.width"
     :height="img.height"
     :format="img.format"
     :quality="img.quality"
     :loading="img.loading"
+    @error="handleError"
     crossorigin="anonymous"
     />
     <!-- :nonce="nonce" -->
