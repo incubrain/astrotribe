3398a1d9fff1f245efbf07f3e705d09780631d3e
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-05-10 12:48:04 +0530

    db: update migration files

--- supabase/migrations/20240502105623_initial_migration_clean.sql
+++ supabase/migrations/20240502105623_initial_migration_clean.sql
@@ -1,4 +1,33 @@
-create extension if not exists "vector" with schema "public" version '0.5.1';
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+CREATE SCHEMA IF NOT EXISTS "public";
+
+ALTER SCHEMA "public" OWNER TO "postgres";
+
+create EXTENSION if not exists "vector" with schema "public" version '0.6.2';
+
+CREATE EXTENSION IF NOT EXISTS "pgsodium" WITH SCHEMA "pgsodium";
+
+CREATE EXTENSION IF NOT EXISTS "pg_graphql" WITH SCHEMA "graphql";
+
+CREATE EXTENSION IF NOT EXISTS "pg_stat_statements" WITH SCHEMA "extensions";
+
+CREATE EXTENSION IF NOT EXISTS "pgcrypto" WITH SCHEMA "extensions";
+
+CREATE EXTENSION IF NOT EXISTS "pgjwt" WITH SCHEMA "extensions";
+
+CREATE EXTENSION IF NOT EXISTS "supabase_vault" WITH SCHEMA "vault";
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "extensions";
 
 create type "public"."access_level" as enum ('Viewer', 'Editor', 'Admin', 'Super Admin');
 
