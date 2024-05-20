c2f3b42f3ccdfe9b677aa6de6282b7002d754072
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-05-20 13:49:02 +0530

    fix: blog categories & button url fixed

--- components/blog/BlogFilter.vue
+++ components/blog/BlogFilter.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import { CATEGORIES } from '~/types/articles'
 import type { ArticleCategoriesT } from '~/types/articles'
+import { useChangeCase } from '@vueuse/integrations/useChangeCase'
 
 const selectedCategory = ref<ArticleCategoriesT>(
   String(useRoute().params.category) as ArticleCategoriesT
 )
-const upperFirstCategory = (cat: string) => cat.charAt(0).toUpperCase() + cat.slice(1)
+const formatCategory = (cat: string) => useChangeCase(cat, 'capitalCase').value
 
 // !todo: low priority - add a search bar to filter articles
 // !todo: low priority - color code the categories
@@ -15,21 +16,20 @@ const upperFirstCategory = (cat: string) => cat.charAt(0).toUpperCase() + cat.sl
 </script>
 
 <template>
-  <PrimeCard class="space-y-4 h-auto md:rounded-md">
-    <template #content>
-      <div class="flex gap-4 flex-wrap">
-        <PrimeButton
-          v-for="cat in CATEGORIES"
-          :key="`astronera-blog-${cat}`"
-          color="primary"
-          :aria-label="`${cat} articles`"
-          :variant="selectedCategory === cat ? 'solid' : 'outline'"
-          :to="`/blog/${cat}`"
-          :label="upperFirstCategory(cat)"
-          size="sm"
-          class="cursor-pointer"
-        />
-      </div>
-    </template>
-  </PrimeCard>
+  <div class="space-y-4 h-auto md:rounded-md">
+    <div class="flex gap-4 flex-wrap items-center">
+      <h3 class="text-xl font-semibold h-full flex items-center leading-none"> Categories: </h3>
+      <PrimeButton
+        v-for="cat in CATEGORIES"
+        :key="`astronera-blog-${cat}`"
+        color="primary"
+        :aria-label="`${formatCategory(cat)} articles`"
+        :variant="selectedCategory === cat ? 'solid' : 'outline'"
+        :to="`/blog/${cat}`"
+        :label="formatCategory(cat)"
+        size="small"
+        class="cursor-pointer"
+      />
+    </div>
+  </div>
 </template>
