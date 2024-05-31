1928fb9b6d6d7f35ade1eceebe31473869e8b05a
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-05-31 07:25:55 +0530

    partial: social media routes init

--- server/utils/getYoutubeContent.ts
+++ server/utils/getYoutubeContent.ts
@@ -1,3 +1,5 @@
+import { useServerError } from './base.error-handler'
+
 interface YouTubeVideo {
   title: string
   description: string
@@ -12,16 +14,17 @@ interface YouTubeProfile {
   videos: YouTubeVideo[]
 }
 
+const YOUTUBE_API_URL = 'https://www.googleapis.com/youtube/v3/'
+const GOOGLE_API_KEY = useRuntimeConfig().googleApiKey
+
 export async function getYoutubeContent(username: string): Promise<YouTubeProfile | null> {
-  const apiKey = useRuntimeConfig().googleApiKey
-  1
   try {
     // Fetch the channel details by username
-    const channelResponse = await $fetch(`https://www.googleapis.com/youtube/v3/channels`, {
+    const channelResponse = await $fetch(`${YOUTUBE_API_URL}channels`, {
       params: {
         forHandle: username,
         part: 'snippet,contentDetails,statistics',
-        key: apiKey
+        key: GOOGLE_API_KEY
       }
     })
 
@@ -36,12 +39,12 @@ export async function getYoutubeContent(username: string): Promise<YouTubeProfil
     const uploadsPlaylistId = channel.contentDetails.relatedPlaylists.uploads
 
     // Fetch the latest videos from the uploads playlist
-    const videosResponse = await $fetch(`https://www.googleapis.com/youtube/v3/playlistItems`, {
+    const videosResponse = await $fetch(`${YOUTUBE_API_URL}playlistItems`, {
       params: {
         part: 'id,snippet,contentDetails',
         maxResults: 12,
         playlistId: uploadsPlaylistId,
-        key: apiKey
+        key: GOOGLE_API_KEY
       }
     })
 
@@ -66,4 +69,46 @@ export async function getYoutubeContent(username: string): Promise<YouTubeProfil
   }
 }
 
-// This function can now be called within your server routes or API routes in Nuxt.
+export function useYoutubeChannel(channelId: string) {
+  const errors = useServerError('getYoutubeChannelAnalytics')
+
+  async function fetchChannelStatistics() {
+    const url = `${YOUTUBE_API_URL}channels?part=statistics&id=${channelId}&key=${GOOGLE_API_KEY}`
+    const response = await $fetch(url)
+    const data = errors.handleFetchError({
+      response,
+      devMessage: `Failed to fetch channel statistics for ${channelId}`,
+      userMessage: `Failed to fetch statistics for channel ${channelId}`
+    })
+    return data.items[0].statistics
+  }
+
+  async function fetchChannelVideos() {
+    const url = `${YOUTUBE_API_URL}search?key=${GOOGLE_API_KEY}&channelId=${channelId}&part=snippet,id&order=date&maxResults=5`
+    const response = await $fetch(url)
+    const data = errors.handleFetchError({
+      response,
+      devMessage: `Failed to fetch channel videos for ${channelId}`,
+      userMessage: `Failed to fetch videos for channel ${channelId}`
+    })
+
+    return data.items
+  }
+
+  async function fetchVideoStatistics(videoId: string) {
+    const url = `${YOUTUBE_API_URL}videos?part=statistics&id=${videoId}&key=${GOOGLE_API_KEY}`
+    const response = await $fetch(url)
+    const data = errors.handleFetchError({
+      response,
+      devMessage: `Failed to fetch video statistics for ${channelId}`,
+      userMessage: `Failed to fetch video statistics for channel ${channelId}`
+    })
+    return data.items[0].statistics
+  }
+
+  return {
+    fetchChannelStatistics,
+    fetchChannelVideos,
+    fetchVideoStatistics
+  }
+}
