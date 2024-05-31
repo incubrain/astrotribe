68ef5bf4af4cbc49379c5f245d2751d8c98649d4
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-05-31 07:25:55 +0530

    partial: social media routes init

---
+++ server/api/social/instagram/analytics.ts
@@ -0,0 +1,32 @@
+import { defineEventHandler, getQuery } from 'h3'
+import { useInstagramAnalytics, useLogger, useServerError } from '#imports'
+
+const PREFIX = 'social/instagram/insights'
+
+export default defineEventHandler(async (event) => {
+  const log = useLogger(PREFIX)
+  const errors = useServerError(PREFIX)
+
+  const { userId } = getQuery(event) as { userId: string }
+
+  log.info(`userId: ${userId}`)
+
+  try {
+    const instagram = useInstagramAnalytics()
+    const businessAnalytics = await instagram.fetchBusinessAnalytics(userId)
+
+    log.info(businessAnalytics)
+
+    return {
+      status: 200,
+      message: 'Profile businessAnalytics returned from Instagram',
+      data: businessAnalytics
+    }
+  } catch (error: any) {
+    errors.handleError({
+      error,
+      devMessage: 'Error fetching Instagram insights',
+      userMessage: 'Error fetching Instagram insights'
+    })
+  }
+})
