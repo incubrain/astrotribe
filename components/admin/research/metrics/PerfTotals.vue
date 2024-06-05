86ff13f5183ba4cd8a34e8fe8aea5ae63ed1e376
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-06-05 08:40:31 +0530

    feat: admin research metrics

---
+++ components/admin/research/metrics/PerfTotals.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+// need to have the totals calculated via sql
+
+
+const { chunk } = useResearchMetrics()
+
+const researchStore = useResearchStore()
+const { researchMetrics } = storeToRefs(researchStore)
+
+const performanceMetrics = ref({
+  avgProcessingTimePerChunk: calculateAverageProcessingTime(metrics.value.performance, totalChunks);
+  memoryUsagePerChunk: calculateMemoryUsagePerChunk(metrics.value.performance, totalChunks);
+  peakMemoryUsage: calculatePeakMemoryUsage(memoryUsageOverTime);
+  memoryEfficiency: calculateMemoryEfficiency(metrics.value.performance, totalChunks);
+  timeEfficiency: calculateTimeEfficiency(metrics.value.performance, totalChunks);
+  resourceUsageIndex: calculateResourceUsageIndex(memoryEfficiency, timeEfficiency);
+});
+
+</script>
+
+<template>
+  <div> </div>
+</template>
+
+<style scoped></style>
