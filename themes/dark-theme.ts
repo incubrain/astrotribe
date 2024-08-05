e77309f48b5669748d7dd1bc113b6c1436023190
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-08-05 19:14:25 +0530

    chore: bump primevue

--- themes/dark-theme.ts
+++ themes/dark-theme.ts
@@ -1,10 +1,24 @@
-import { definePreset, palette } from '@primevue/themes'
+import { definePreset, palette, updatePrimaryPalette } from '@primevue/themes'
 import Aura from '@primevue/themes/aura'
 
 const primaryColor = palette('#2471b3')
 
+updatePrimaryPalette(primaryColor)
+
 export const DarkPreset = definePreset(Aura, {
   semantic: {
-    primary: primaryColor
+    primary: {
+      50: primaryColor[50],
+      100: primaryColor[100],
+      200: primaryColor[200],
+      300: primaryColor[300],
+      400: primaryColor[400],
+      500: primaryColor[500],
+      600: primaryColor[600],
+      700: primaryColor[700],
+      800: primaryColor[800],
+      900: primaryColor[900],
+      950: primaryColor[950]
+    }
   }
 })
