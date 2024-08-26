2d0eb6a97fb58a34661d52cd1e612c7b68bd0565
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-08-26 13:50:53 +0530

    feat: payments setup

---
+++ server/utils/generateServerToken.ts
@@ -0,0 +1,10 @@
+import jwt from 'jsonwebtoken'
+import { useRuntimeConfig } from '#imports'
+
+export function generateServerToken() {
+  const config = useRuntimeConfig()
+  return jwt.sign({ sender: 'AstronEra' }, config.scraperKey, {
+    algorithm: 'HS256',
+    expiresIn: '1h'
+  })
+}
