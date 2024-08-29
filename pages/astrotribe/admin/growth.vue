ef1a474711657614cba624b56996a255c32cbb28
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-08-29 20:39:53 +0530

    feat: table growth stats

---
+++ pages/astrotribe/admin/growth.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+definePageMeta({
+  layoutTransition: false,
+  name: 'Growth',
+  middleware: 'is-admin'
+})
+</script>
+
+<template>
+  <div class="relative h-full max-h-full">
+    <!-- <AdminGrowthMetrics /> -->
+    <AdminTableGrowth />
+  </div>
+</template>
+
+<style scoped></style>
