b0a308df4c7fcefb3378c552b57b0e7f81656ac2
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-09-12 13:06:03 +0530

    ci: setup simple ci with bundle size tracking and lighthouse tracking

---
+++ lighthouserc.cjs
@@ -0,0 +1,23 @@
+/** @type {import('@lhci/cli').ConfigFile} */
+module.exports = {
+  ci: {
+    collect: {
+      numberOfRuns: 3,
+      startServerCommand: 'npm run preview',
+      url: ['http://localhost:3000'],
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['warn', { minScore: 0.5 }],
+        'categories:accessibility': ['warn', { minScore: 0.7 }],
+        'categories:best-practices': ['warn', { minScore: 0.7 }],
+        'categories:seo': ['warn', { minScore: 0.7 }],
+      },
+    },
+    upload: {
+      target: 'filesystem',
+      outputDir: './ci/reports/lighthouse',
+      reportFilenamePattern: '%%DATETIME%%-lighthouse.json',
+    },
+  },
+}
