de3c2dd6b5f8d3457ec54d5531c144e38933232c
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-09-20 13:12:26 +0530

    fix: remove mermaid

--- pages/test.vue
+++ pages/test.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="wrapper pt-36">
-    <div class="flex h-full w-full flex-col gap-8 p-4">
-      <AppMermaid>
-        sequenceDiagram; participant User; participant AppServer; participant Supabase;
-        User->>AppServer: Submit email and password; AppServer->>Supabase: Create new user with
-        email and password; Supabase->>AppServer: User created; AppServer->>Supabase: Assign
-        plan/role to app_metadata; Supabase->>User: Send confirmation email; User->>Supabase:
-        Confirm email; Supabase->>AppServer: Email confirmed; AppServer->>User: Registration
-        complete;
-      </AppMermaid>
-    </div>
+    <div class="flex h-full w-full flex-col gap-8 p-4"> </div>
   </div>
 </template>
 
