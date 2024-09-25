42ceb04f7f83973c65ba4a79240edfded4b8596c
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-09-25 11:52:53 +0530

    chore: cleanup old code

--- apps/main-app/server/utils/openai/openaiClient.ts
+++
@@ -1,8 +0,0 @@
-import OpenAI from 'openai/index.mjs'
-
-const openaiClient = new OpenAI({
-  organization: process.env.OPENAI_ORG_ID,
-  project: process.env.OPENAI_PROJECT_ID,
-})
-
-export default openaiClient
