bf51133229b971379f6263cca326aced17d3f2bd
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-09-26 10:51:16 +0530

    fix: linting & prettier working together, single eslint root file

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
-import { createConfigForNuxt } from '@nuxt/eslint-config/flat';
+import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default createConfigForNuxt({
   features: {
@@ -8,38 +9,37 @@ export default createConfigForNuxt({
       quotes: 'single',
     },
   },
-}).append({
-  files: ['**/*.{js,ts,vue}'],
-  rules: {
-    'vue/multi-word-component-names': 'off',
-    'no-global-assign': ['off', { exceptions: ['Object'] }],
-    'no-undefined': 'off',
-    'no-console': 'off',
-    'no-new': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    quotes: [
-      'error',
-      'single',
-      { allowTemplateLiterals: true, avoidEscape: true },
+})
+  .append({
+    files: ['**/*.{js,ts,vue}'],
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'no-global-assign': ['off', { exceptions: ['Object'] }],
+      'no-undefined': 'off',
+      'no-console': 'off',
+      'no-new': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'quote-props': ['error', 'consistent-as-needed'],
+      'quotes': ['error', 'single', { allowTemplateLiterals: true, avoidEscape: true }],
+      'arrow-parens': ['error', 'always'],
+      '@stylistic/arrow-parens': ['error', 'always'],
+      '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+      'object-curly-newline': 'off',
+      'curly': 'off',
+      'no-undef': 'off',
+      'space-before-function-paren': 'off',
+      'vue/html-self-closing': 'off',
+    },
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/build/**',
+      '**/.nuxt/**',
+      '**/public/**',
+      './theme/**',
+      '**/theme/**',
+      '**/.output/**',
     ],
-    'arrow-parens': ['error', 'always'],
-    '@stylistic/arrow-parens': ['error', 'always'],
-    '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: true }],
-    'object-curly-newline': 'off',
-    curly: 'off',
-    'no-undef': 'off',
-    'space-before-function-paren': 'off',
-    'vue/html-self-closing': 'off',
-  },
-  ignores: [
-    '**/node_modules/**',
-    '**/dist/**',
-    '**/build/**',
-    '**/.nuxt/**',
-    '**/public/**',
-    './theme/**',
-    '**/theme/**',
-    '**/.output/**',
-  ],
-});
+  })
+  .append(eslintConfigPrettier)
---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  proseWrap: 'always',
+  printWidth: 100,
+  htmlWhitespaceSensitivity: 'strict',
+  singleAttributePerLine: true,
+  quoteProps: 'consistent',
+}
