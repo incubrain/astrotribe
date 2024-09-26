6b112abfabd664b50e328f92860609be8269ee83
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-09-26 11:14:39 +0530

    fix: tsconfigs correctly configured

--- apps/auth-service/vitest.config.ts
+++ apps/auth-service/vitest.config.ts
@@ -1,14 +1,13 @@
 /// <reference types='vitest' />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin'
 
 export default defineConfig({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/website',
 
-  plugins: [vue(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+  plugins: [vue(), nxCopyAssetsPlugin(['*.md'])],
 
   // Uncomment this if you are using workers.
   // worker: {
