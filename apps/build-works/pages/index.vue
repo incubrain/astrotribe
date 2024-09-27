9ded37446060fc34e32944a6b0ec7ec2a8e93714
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-09-27 08:50:07 +0530

    chore: add app that builds with vueuse

---
+++ apps/build-works/pages/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <p>testing</p>
+    <PrimeButton> hellow world </PrimeButton>
+    <PrimeCard class="p-4 bg-red-300" />
+  </div>
+</template>
+
+<style></style>
