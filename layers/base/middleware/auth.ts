787ff07631b13d8a00f635ddb97a59a340ee2346
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-09-28 10:49:32 +0530

    feat: cors compliant auth middleware

--- layers/base/middleware/auth.ts
+++ layers/base/middleware/auth.ts
@@ -1,10 +1,14 @@
-import { defineNuxtRouteMiddleware, navigateTo } from '#app'
+import { defineNuxtRouteMiddleware, navigateTo, useCookie } from '#app'
+import { useRuntimeConfig } from '#imports'
 
 export default defineNuxtRouteMiddleware((to, from) => {
-  const loginUrl = useRuntimeConfig().public.loginUrl
+  const config = useRuntimeConfig()
+  const loginUrl = config.public.loginUrl
   const user = useSupabaseUser()
+  const authToken = useCookie('auth_token')
+  const authTokenFallback = useCookie('auth_token_fallback')
 
-  if (!user.value) {
+  if (!user.value && !authToken.value && !authTokenFallback.value) {
     console.log('User not logged in')
     return navigateTo(loginUrl, { external: true })
   }
