ed8d4957776b858c7a133d4a2b120920f145f895
ruturajpokemon <ruturajpokemon1@gmail.com>
2024-10-03 14:21:55 +0530

    ui: fix error toast message partially hidden

--- theme/toast/index.js
+++ theme/toast/index.js
@@ -56,7 +56,7 @@ export default {
       // Font and Text
       'text-base leading-none',
       'ml-2',
-      'flex flex-col w-full overflow-hidden',
+      'flex flex-col w-full overflow-visible',
     ],
   },
   summary: {
