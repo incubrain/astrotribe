e8ccac3886ac87c1a15bd7d26933c9315cf835ff
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-10-04 10:44:39 +0530

    fix: add astronera subdomains to cookie and cors

--- apps/admin-dashboard/middleware/auth.global.ts
+++ apps/admin-dashboard/middleware/auth.global.ts
@@ -7,15 +7,13 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
 
   try {
     const user = useSupabaseUser()
-    const authToken = useCookie('auth_token')
-    const authTokenFallback = useCookie('auth_token_fallback')
 
-    if (!user.value && !authToken.value && !authTokenFallback.value) {
+    if (!user.value) {
       console.log('USER_NOT_LOGGED_IN', aeAuthUrl, aeLoginUrl)
 
       return navigateTo(String(`${aeAuthUrl}${aeLoginUrl}`), { external: true })
     } else {
-      console.log('USER_LOGGED_IN', user.value, authToken.value, authTokenFallback.value)
+      console.log('USER_LOGGED_IN', user.value)
     }
   } catch (error: any) {
     console.error('redirect error', `${aeAuthUrl}${aeLoginUrl}`)
