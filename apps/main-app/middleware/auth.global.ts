a69918a0e499ab203374b2ea46de8fe6730f8151
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-10-04 15:31:21 +0530

    .

--- apps/main-app/middleware/auth.global.ts
+++ apps/main-app/middleware/auth.global.ts
@@ -4,15 +4,15 @@ import { useRuntimeConfig } from '#imports'
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const config = useRuntimeConfig()
   const { aeLoginUrl, aeAuthUrl } = config.public
-  const user = await useSupabaseUser()
+  const session = useSupabaseSession()
 
-  console.log('waiting for timeout', user.value)
+  console.log('waiting for timeout', session.value)
   await new Promise((resolve) => setTimeout(resolve, 10000))
 
-  if (!user.value) {
+  if (!session.value) {
     console.log('USER_NOT_LOGGED_IN', aeAuthUrl, aeLoginUrl)
     return navigateTo(String(`${aeAuthUrl}${aeLoginUrl}`), { external: true })
   } else {
-    console.log('USER_LOGGED_IN', user.value)
+    console.log('USER_LOGGED_IN', session.value)
   }
 })
