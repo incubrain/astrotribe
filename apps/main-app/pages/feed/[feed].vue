dcb3e10f4b3fe80f152bb5900b5f2abd6a755f26
ruturajpokemon <ruturajpokemon1@gmail.com>
2024-10-23 14:57:59 +0530

    ui: add news for the custom feeds

--- apps/main-app/pages/feed/[feed].vue
+++ apps/main-app/pages/feed/[feed].vue
@@ -14,6 +14,27 @@ const getCategories = async () => {
 }
 
 onMounted(getCategories)
+
+const domainKey = 'news'
+const { store, loadMore, refresh } = useSelectData<News>('news', {
+  columns:
+    'id, title, body, published_at, created_at, description, category_id, author, url, keywords, featured_image, company_id, companies(*)',
+  filters: {
+    category_id: { eq: '5', or: '4' },
+    // content_status: { eq: 'draft' },
+    // body: { neq: 'null' },
+  },
+  orderBy: { column: 'created_at', ascending: false },
+  initialFetch: true,
+  pagination: { page: 1, limit: 20 },
+})
+
+const { items: proxyNews } = storeToRefs(store)
+
+const news = computed(() => proxyNews.value.map((item) => toRaw(item)))
+console.log('NEWS', news)
+const loading = useLoadingStore()
+const isLoading = computed(() => loading.isLoading(domainKey))
 </script>
 
 <template>
@@ -26,4 +47,20 @@ onMounted(getCategories)
     size="small"
     class="m-2"
   />
+  <div>
+    <IBInfiniteScroll @update:scroll-end="loadMore()">
+      <div
+        class="mx-auto w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:col-start-2 gap-4 md:gap-8 p-4 md:p-8 max-w-[940px]"
+      >
+        <NewsCard
+          v-for="(item, i) in news"
+          :key="`news-post-${i}`"
+          :news="item"
+        />
+        <NewsCardSkeleton v-show="isLoading" />
+      </div>
+    </IBInfiniteScroll>
+  </div>
 </template>
+
+<style scoped></style>
