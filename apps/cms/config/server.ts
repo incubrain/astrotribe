54ac3d1ca7f84da85363cdd4f1228972f0cd046e
ruturajpokemon <ruturajpokemon1@gmail.com>
2024-10-27 17:10:53 +0530

    .

--- apps/cms/config/server.ts
+++ apps/cms/config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL'),
+  url: env('PUBLIC_URL', 'https://cms.astronera.org'),
   app: {
     keys: env.array('APP_KEYS'),
   },
