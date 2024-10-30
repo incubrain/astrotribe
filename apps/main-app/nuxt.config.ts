3e3206db429092ec16c68b8506436cd07f9b1899
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-10-30 15:34:35 +1300

    feat: pwa for main-app

--- apps/main-app/nuxt.config.ts
+++ apps/main-app/nuxt.config.ts
@@ -35,6 +35,7 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@nuxtjs/mdc',
     '@primevue/nuxt-module',
+    '@vite-pwa/nuxt',
   ],
 
   experimental: {
@@ -78,6 +79,69 @@ export default defineNuxtConfig({
 
   tiptap: {
     prefix: 'Tiptap',
+  },
+
+  pwa: {
+    registerType: 'autoUpdate', // Automatically update the service worker
+    manifest: {
+      screenshots: [
+        {
+          src: '/screenshots/screenshot1.png',
+          sizes: '640x480',
+          type: 'image/png',
+        },
+        // Add more screenshots
+      ],
+      lang: 'en',
+      dir: 'ltr',
+      name: 'AstronEra',
+      short_name: 'Astron',
+      description: 'Your Daily Astronomical Dose',
+      theme_color: '#000000',
+      background_color: '#000000',
+      display: 'standalone',
+      orientation: 'portrait',
+      scope: '/',
+      start_url: '/',
+      icons: [
+        {
+          src: '/favicons/web-app-manifest-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+        {
+          src: '/favicons/web-app-manifest-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+      ],
+    },
+    workbox: {
+      navigateFallback: '/offline',
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp(`^${process.env.SUPABASE_URL}/.*$`),
+          handler: 'NetworkFirst',
+          method: 'GET',
+
+          options: {
+            cacheName: 'supabase-api-cache',
+            expiration: {
+              maxEntries: 100,
+              maxAgeSeconds: 60 * 60, // 1 hour
+            },
+            cacheableResponse: {
+              statuses: [0, 200],
+            },
+          },
+        },
+      ],
+    },
+    devOptions: {
+      enabled: true, // Enable PWA in development mode
+    },
   },
 
   ...runtimeConfig,
