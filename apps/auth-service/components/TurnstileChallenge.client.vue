8057f784ccd1f965c74445a3f2eb9a87ec99d63c
ruturajpokemon <ruturajpokemon1@gmail.com>
2024-12-09 10:55:34 +0530

    fix: clear turnstile cookie before resetting

--- apps/auth-service/components/TurnstileChallenge.client.vue
+++ apps/auth-service/components/TurnstileChallenge.client.vue
@@ -70,6 +70,12 @@ const onExpired = () => {
 }
 
 const reset = () => {
+  const cloudflareCookies = ['__cf_bm', 'cf_clearance', '__cfwaitingroom']
+
+  cloudflareCookies.forEach((cookieName) => {
+    document.cookie = `${cookieName}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`
+  })
+
   if (window.turnstile) {
     window.turnstile.reset()
   }
