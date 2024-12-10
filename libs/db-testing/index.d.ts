eb6f5cc216fdae3bf2ff710b9471be0661372207
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-12-10 15:17:34 +0530

    fix: update nx project.json's to depend on layers/advert

--- libs/db-testing/index.d.ts
+++ libs/db-testing/index.d.ts
@@ -2,7 +2,5 @@
 export type * from './src/types'
 
 // Re-export function implementations
-export { createTestRunner } from './src/test-runner'
 export { createRLSHandlers } from './src/msw-handlers'
 export { executeTest } from './src/test-helpers'
-export { generateTestsFromPermissions } from './src/test-generator'
