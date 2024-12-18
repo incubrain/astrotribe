b0697fb19399aa6ec9639c94252a266fb0864510
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-12-18 21:26:14 +0530

    chore: remove unused env file

--- apps/api/src/core/services/cache.service.ts
+++ apps/api/src/core/services/cache.service.ts
@@ -1,23 +1,23 @@
 // services/cache.service.ts
-import { CACHE_MANAGER } from "@nestjs/cache-manager";
-import { Inject, Injectable } from "@nestjs/common";
-import { Cache } from "cache-manager";
-import { CustomLogger } from "../logger/custom.logger";
+import { CACHE_MANAGER } from '@nestjs/cache-manager'
+import { Cache } from 'cache-manager'
+import { Inject, Injectable } from '@nestjs/common'
+import { CustomLogger } from '../logger/custom.logger'
 
 @Injectable()
 export class CacheService {
   constructor(
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
-    private logger: CustomLogger
+    private logger: CustomLogger,
   ) {
-    this.logger.setContext("CacheService");
+    this.logger.setContext('CacheService')
   }
 
   async get<T>(key: string): Promise<T | undefined> {
-    return this.cacheManager.get<T>(key);
+    return this.cacheManager.get<T>(key)
   }
 
   async set(key: string, value: any, ttl?: number): Promise<void> {
-    await this.cacheManager.set(key, value, ttl);
+    await this.cacheManager.set(key, value, ttl)
   }
 }
