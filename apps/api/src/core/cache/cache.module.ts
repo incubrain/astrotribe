cdd51ed018a1334b3760dcbcc8ee31e07535bfc7
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2024-12-30 13:09:41 +0530

    fix: redis envs for api

--- apps/api/src/core/cache/cache.module.ts
+++ apps/api/src/core/cache/cache.module.ts
@@ -8,50 +8,29 @@ import IORedis from 'ioredis'
 @Module({
   imports: [ConfigModule],
   providers: [
-    // 1) Redis client for Bull (jobs queue)
-    {
-      provide: 'REDIS_BULL',
-      useFactory: (config: ConfigService) => {
-        return new IORedis({
-          host: config.get<string>('app.redisBullHost') || 'redis.railway.internal',
-          port: config.get<number>('app.redisBullPort') || 6379,
-          password: config.get<string>('app.redisBullPassword'),
-          username: config.get<string>('app.redisBullUser'),
-          family: 0,
-          retryStrategy: (times: number) => {
-            const delay = Math.min(times * 50, 2000)
-            return delay
-          },
-          maxRetriesPerRequest: null,
-        })
-      },
-      inject: [ConfigService],
-    },
-
     // 2) Redis client for cache/metrics
     {
       provide: 'REDIS_CACHE',
       useFactory: (config: ConfigService) => {
         return new IORedis({
-          host: config.get<string>('app.redisCacheHost') || 'redis.railway.internal',
-          port: config.get<number>('app.redisCachePort') || 6379,
-          password: config.get<string>('app.redisCachePassword'),
-          username: config.get<string>('app.redisBullUser'),
+          host: config.get<string>('app.redis.host') || 'redis.railway.internal',
+          port: config.get<number>('app.redis.port') || 6379,
+          password: config.get<string>('app.redis.password'),
+          username: config.get<string>('app.redis.username'),
         })
       },
       inject: [ConfigService],
     },
   ],
-  exports: ['REDIS_BULL', 'REDIS_CACHE'],
+  exports: ['REDIS_CACHE'],
 })
 export class RedisModule implements OnApplicationShutdown {
   constructor(
-    @Inject('REDIS_BULL') private bullClient: Redis,
     @Inject('REDIS_CACHE') private cacheClient: Redis,
   ) {}
 
   async onApplicationShutdown() {
     // Alternative place to close connections if desired
-    await Promise.all([this.bullClient.quit(), this.cacheClient.quit()])
+    await Promise.all([this.cacheClient.quit()])
   }
 }
