6ec3ce95d91f5ef4087b9cc5cc727489176c64d1
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2025-01-08 11:04:23 +0530

    feat: ads preview for admin dashboard

--- apps/admin/pages/advertising.vue
+++ apps/admin/pages/advertising.vue
@@ -1,9 +1,43 @@
+<script setup lang="ts">
+definePageMeta({
+  layoutTransition: false,
+  name: 'Advertising',
+})
+
+const tabs = [
+  {
+    title: 'Ads Dashboard',
+    slotName: 'dashboard',
+    value: 'dashboard',
+  },
+  {
+    title: 'Ads Preview',
+    slotName: 'preview',
+    value: 'preview',
+  },
+]
+</script>
+
 <template>
-  <div>
-    <AdsAdminDashboard />
+  <div class="min-h-screen">
+    <!-- Tabs Container -->
+    <div class="h-[calc(100vh-73px)]">
+      <!-- 73px accounts for header height -->
+      <IBTabView :tabs="tabs">
+        <template #start>
+          <h1 class="text-xl font-semibold text-white">Error Monitoring</h1>
+        </template>
+
+        <template #dashboard>
+          <AdsAdminDashboard />
+        </template>
+
+        <template #preview>
+          <AdsAdminPreview />
+        </template>
+      </IBTabView>
+    </div>
   </div>
 </template>
-
-<script setup lang="ts"></script>
 
 <style scoped></style>
