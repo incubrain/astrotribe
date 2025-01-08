42ff8b2ba9931f3801969ddfaea63a87f05901a6
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2025-01-08 20:18:31 +0530

    feat: prefix api routes with domain, eg. api/v1/[content]/endpoint

--- apps/api/src/core/config/domain-config.ts
+++ apps/api/src/core/config/domain-config.ts
@@ -1,13 +1,7 @@
 // core/config/domain-config.ts
-import { DynamicModule } from '@nestjs/common'
 import { createCentralizedLogger, Service, DomainsForService } from '@ib/logger'
-
-import { ConfigModule } from '@nestjs/config'
-import { Module } from '@nestjs/common'
+import { DynamicModule, MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
 import { CustomLogger } from '@core/logger/custom.logger'
-import { CoreModule } from '../core.module'
-import { PrismaModule } from '../modules/prisma.module'
-import { PermissionModule } from '../modules/permission.module'
 
 // Define the options interface
 export interface DomainModuleOptions {
@@ -16,24 +10,25 @@ export interface DomainModuleOptions {
   // Add other options as needed
 }
 
-// Define a base module class
-@Module({})
-class DomainModule {}
-
-// core/config/domain-config.ts
 export function createDomainModule(
   domainName: DomainsForService<Service.API>,
   options: DomainModuleOptions,
 ): DynamicModule {
+  @Module({})
+  class DomainModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+      consumer.apply().forRoutes(`/api/${domainName}/*`) // Add a prefix dynamically
+    }
+  }
+
   return {
     module: DomainModule,
     providers: [
       {
         provide: CustomLogger,
         useFactory: () => {
-          // Create a CustomLogger instance and set the domain once here.
           const logger = new CustomLogger()
-          logger.setDomain(domainName) // or whatever domain naming you prefer
+          logger.setDomain(domainName)
           return logger
         },
       },
