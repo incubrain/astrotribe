177fb46c686c47ca717e4791a2db8f18f2155db4
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2025-01-10 22:37:40 +0530

    chore: update libs in build:all

--- ci/1.build-with-logs.ts
+++ ci/1.build-with-logs.ts
@@ -53,7 +53,7 @@ async function runBuildWithLogs(projectPath: string): Promise<void> {
   fs.writeFileSync(errorLogFile, '')
   fs.writeFileSync(fullLogFile, '')
 
-  const libNames = ['db-testing', 'logger']
+  const libNames = ['db-testing', 'logger', 'cache', 'core', 'helpers']
 
   let projectPrefix = '@astronera'
 
