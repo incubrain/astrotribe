c2cd7dc238d1a1c9952be26d4d71d96d24ff8747
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2025-01-23 10:29:16 +0530

    chore: remove monitoring dir

--- apps/monitoring/layouts/default.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div class="relative flex h-full min-h-screen w-full items-center justify-start">
-    <slot />
-  </div>
-</template>
-
-<style scoped></style>
--- apps/monitoring/pages/not-authorized.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div> you are not authorized to see this </div>
-</template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
