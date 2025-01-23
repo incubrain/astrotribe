2664710badea13089db46dda8177d97a0edcb8cf
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2025-01-23 10:03:06 +0530

    chore: move shared conf into shared/ dir

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -3,35 +3,35 @@ import { defineWorkspace } from 'vitest/config'
 
 export default defineWorkspace([
   {
-    extends: './vitest.shared.config.ts',
+    extends: './shared/vitest.shared.config.ts',
     test: {
       name: 'api',
       root: './api',
     },
   },
   {
-    extends: './vitest.shared.config.ts',
+    extends: './shared/vitest.shared.config.ts',
     test: {
       name: 'website',
       root: './apps/website',
     },
   },
   {
-    extends: './vitest.shared.config.ts',
+    extends: './shared/vitest.shared.config.ts',
     test: {
       name: 'app',
       root: './apps/app',
     },
   },
   {
-    extends: './vitest.shared.config.ts',
+    extends: './shared/vitest.shared.config.ts',
     test: {
       name: 'admin',
       root: './apps/admin',
     },
   },
   {
-    extends: './vitest.shared.config.ts',
+    extends: './shared/vitest.shared.config.ts',
     test: {
       name: '@astronera/auth',
       root: './apps/auth',
