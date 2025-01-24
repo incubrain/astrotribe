daeb6796b9924815ae0535f38e328f8d546ac291
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2025-01-24 17:45:24 +0530

    chore: ignore openapi code

---
+++ libs/db/src/prisma/schema-to-openapi.ts
@@ -0,0 +1,14 @@
+// prisma/schema-to-openapi.ts
+import { generateOpenAPI } from 'prisma-generator-openapi'
+
+export async function generateAPISpec() {
+  const spec = await generateOpenAPI({
+    prismaSchema: './prisma/schema.prisma',
+    output: './specs/v1/generated-schemas.yaml',
+    defaultResponseFormat: 'json',
+    info: {
+      title: 'Astronera API',
+      version: '1.0.0',
+    },
+  })
+}
