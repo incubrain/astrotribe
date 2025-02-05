6ea8eb0b427cb510c06b99c898e885749b6fce0d
Ruturaj <ruturajpokemon1@gmail.com>
2025-02-05 06:16:26 +0000

    fix: darksky justify center and resize

--- apps/website/components/symposium/SymposiumAbout.vue
+++ apps/website/components/symposium/SymposiumAbout.vue
@@ -48,13 +48,13 @@
         >
         <li>Highlight dark skies as a fundamental right for every global citizen.</li>
       </ul>
-      <div class="flex gap-2 flex-col md:flex-row">
+      <div class="flex justify-center gap-2 flex-col md:flex-row">
         <IBImage
           :img="{
             src: 'symposium/before_and_after_germany.jpeg',
             alt: 'https://www.instagram.com/darksky_intl?igsh=MWJqOGV1dGNrbGZ1OQ==',
-            width: '360',
-            height: '360',
+            width: '500',
+            height: '500',
           }"
         />
         <IBImage
