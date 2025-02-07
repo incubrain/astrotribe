1db39fa9a891ee67248d14f6f5324ff1bb0d59c0
Ruturaj <ruturajpokemon1@gmail.com>
2025-02-07 06:44:41 +0000

    fix: venue navigation

--- apps/website/components/symposium/SymposiumVenue.vue
+++ apps/website/components/symposium/SymposiumVenue.vue
@@ -20,32 +20,34 @@ const venues = [
 </script>
 
 <template>
-  <CommonTitle :title="{ main: 'Venue' }" />
-  <div class="relative grid grid-cols-1 items-start gap-6 lg:grid-cols-2 xl:gap-12">
-    <div
-      class="border-2 p-2 rounded"
-      v-for="venue in venues"
-    >
-      <CommonTitle
-        class="row-start-1"
-        :title="venue"
-      />
-      <p>{{ venue.about }}</p>
-      <div class="row-start-2 h-full w-full space-y-4 text-left leading-6">
-        <PrimeButton
-          class="gap-2 border-white"
-          as="a"
-          :href="venue.website"
-          target="_blank"
-          variant="outlined"
-        >
-          <Icon
-            :name="venue.icon"
-            :class="`text-${venue.color}-400 group-hover:text-sky-400 transition-colors duration-300`"
-            size="36px"
-          />
-          {{ venue.button }}
-        </PrimeButton>
+  <div>
+    <CommonTitle :title="{ main: 'Venue' }" />
+    <div class="relative grid grid-cols-1 items-start gap-6 lg:grid-cols-2 xl:gap-12">
+      <div
+        class="border-2 p-2 rounded"
+        v-for="venue in venues"
+      >
+        <CommonTitle
+          class="row-start-1"
+          :title="venue"
+        />
+        <p>{{ venue.about }}</p>
+        <div class="row-start-2 h-full w-full space-y-4 text-left leading-6">
+          <PrimeButton
+            class="gap-2 border-white"
+            as="a"
+            :href="venue.website"
+            target="_blank"
+            variant="outlined"
+          >
+            <Icon
+              :name="venue.icon"
+              :class="`text-${venue.color}-400 group-hover:text-sky-400 transition-colors duration-300`"
+              size="36px"
+            />
+            {{ venue.button }}
+          </PrimeButton>
+        </div>
       </div>
     </div>
   </div>
