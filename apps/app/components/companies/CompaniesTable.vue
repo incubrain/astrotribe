f7ef042c62f1c3ab043fcf92bbe9cb9ca9444e68
ruturajgole <ruturajpokemon1@gmail.com>
2025-02-25 09:56:12 +0000

    fix: government tag

--- apps/app/components/companies/CompaniesTable.vue
+++ apps/app/components/companies/CompaniesTable.vue
@@ -134,7 +134,7 @@ const getIcon = (key: string) => {
         </td>
         <td 
           v-if="company.is_government"
-          class="absolute text-xs left-2 bottom-2 md:top-2 md:right-2 p-2 bg-gray-600 text-black rounded-xl"
+          class="absolute text-xs top-2 right-2 p-2 bg-gray-600 text-black rounded-xl"
           ><h3>Government</h3></td
         >
       </tr>
