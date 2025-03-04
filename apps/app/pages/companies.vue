65aadc51e6a3a8207bfc499202b90282e6a7b0a0
ruturajgole <ruturajpokemon1@gmail.com>
2025-03-04 10:51:42 +0000

    fix: get companies only that are published

--- apps/app/pages/companies.vue
+++ apps/app/pages/companies.vue
@@ -21,7 +21,7 @@ const { store, loadMore } = useSelectData('companies', {
     page: 1,
     limit: 20,
   },
-  filters: { logo_url: { like: '%https%' } },
+  filters: { content_status: { eq: 'published' } },
   initialFetch: true,
   storeKey: 'companiesFeed',
 })
