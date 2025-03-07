74e50f28fdaea92823d59a0d43cb8520f904e31f
ruturajgole <ruturajpokemon1@gmail.com>
2025-03-07 06:39:05 +0000

    ui: move company name above the other content in companies

--- apps/app/components/companies/CompaniesCell.vue
+++ apps/app/components/companies/CompaniesCell.vue
@@ -60,86 +60,74 @@ const getIcon = (key: string) => {
 </script>
 
 <template>
-  <tr
-    class="relative flex p-5 mt-1 mb-1 border border-primary-500 rounded-xl gap-2 text-lg text-white shadow-md"
-  >
-    <td class="px-2 py-3 w-1/5">
-      <div class="flex flex-col items-center">
-        <IBImage
-          :img="{ src: image, alt: company.name, width: '200', height: '200' }"
-          @error="onError"
-          class="rounded-xl bg-white p-2"
-        />
-        <p class="text-center w-full text-wrap">{{ company.name }}</p>
-      </div>
+  <tr class="relative border border-primary-500 rounded-xl flex flex-col p-5 mt-1 mb-1 gap-2 text-lg text-white shadow-md">
+    <!-- Company name at the top -->
+    <td class="px-4 w-full" colspan="3">
+      <h2 class="text-2xl font-bold w-full">{{ company.name }}</h2>
     </td>
-    <td class="px-4 flex flex-col gap-2 justify-between w-3/5 flex-2 py-3 whitespace-wrap max-w-xs text-left"
-      ><p
-        v-if="company.description"
-        class="align-middle"
-        >{{ company.description.slice(0, 1).toUpperCase()
-        }}{{
-          company.description.length <= 240 || currentlyOpen
-            ? company.description.slice(1)
-            : `${company.description.slice(1, 241)}...`
-        }}
-        <p
-          v-if="company.description.length > 240"
-          class="text-primary-300 cursor-pointer"
-          @click="currentlyOpen = !currentlyOpen"
-          >{{ currentlyOpen ? 'Read Less' : 'Read More' }}</p
-        >
-      </p>
-      <p
-        v-else
-        class="w-full min-h-[1em]"
-        >&nbsp;</p
-      >
-      <NuxtLink v-if="company.url" :to="company.url" target="_blank" external>
-        <PrimeButton outlined severity="contrast">Company Website</PrimeButton>
-      </NuxtLink>
-    </td>
-    <td class="flex py-3 w-1/5 flex-1 flex-col">
-      <p v-if="company.founding_year"><b>Founding Year:</b> {{ company.founding_year }}</p>
-      <p v-if="company.city"><b>Location:</b> {{ company.city }}, {{ company.country }}</p>
-      <p v-if="company.category"><b>Category:</b> {{ company.category }}</p>
-      <div
-        v-if="company.social_media"
-        class="flex flex-col md:flex-row mt-auto items-center justify-between"
-      >
-      <div>
-        <NuxtLink
-          v-for="(url, key) in company.social_media"
-          v-show="url && key !== 'id'"
-          :key="company.social_media.id"
-          :to="url"
-          target="_blank"
-          external
-        >
-        <PrimeButton
-          outlined
-        >
-          <Icon
-            :name="getIcon(key)"
-            size="36px"
-            class="text-white"
+    
+    <!-- Main content row -->
+    <div class="flex gap-2 w-full">
+      <!-- Logo column -->
+      <td class="px-2 py-3 w-1/5">
+        <div class="flex flex-col items-center">
+          <IBImage
+            :img="{ src: image, alt: company.name, width: '200', height: '200' }"
+            @error="onError"
+            class="rounded-xl bg-white p-2"
           />
-        </PrimeButton>
-      </NuxtLInk>
+        </div>
+      </td>
+      
+      <!-- Description column -->
+      <td class="px-4 flex flex-col gap-2 justify-between w-3/5 flex-2 py-3 whitespace-wrap max-w-xs text-left">
+        <p v-if="company.description" class="align-middle">
+          {{ company.description.slice(0, 1).toUpperCase() }}{{ company.description.length <= 240 || currentlyOpen
+              ? company.description.slice(1)
+              : `${company.description.slice(1, 241)}...` }}
+          <p v-if="company.description.length > 240" class="text-primary-300 cursor-pointer" @click="currentlyOpen = !currentlyOpen">
+            {{ currentlyOpen ? 'Read Less' : 'Read More' }}
+          </p>
+        </p>
+        <p v-else class="w-full min-h-[1em]">&nbsp;</p>
+        <NuxtLink v-if="company.url" :to="company.url" target="_blank" external>
+          <PrimeButton outlined severity="contrast">Company Website</PrimeButton>
+        </NuxtLink>
+      </td>
+      
+      <!-- Company details column -->
+      <td class="flex py-3 w-1/5 flex-1 flex-col">
+        <p v-if="company.founding_year"><b>Founding Year:</b> {{ company.founding_year }}</p>
+        <p v-if="company.city"><b>Location:</b> {{ company.city }}, {{ company.country }}</p>
+        <p v-if="company.category"><b>Category:</b> {{ company.category }}</p>
+        <div v-if="company.social_media" class="flex flex-col md:flex-row mt-auto items-center justify-between">
+          <div>
+            <NuxtLink
+              v-for="(url, key) in company.social_media"
+              v-show="url && key !== 'id'"
+              :key="company.social_media.id"
+              :to="url"
+              target="_blank"
+              external
+            >
+              <PrimeButton outlined>
+                <Icon :name="getIcon(key)" size="36px" class="text-white" />
+              </PrimeButton>
+            </NuxtLink>
+          </div>
+          <NuxtLink v-if="company.job_url" :to="company.job_url" target="_blank" external>
+            <PrimeButton outlined severity="contrast" class="shiny-button font-bold">Jobs</PrimeButton>
+          </NuxtLink>
+        </div>
+      </td>
     </div>
-      <NuxtLink v-if="company.job_url" :to="company.job_url" target="_blank" external>
-        <PrimeButton outlined severity="contrast" class="shiny-button font-bold">Jobs</PrimeButton>
-      </NuxtLink>
-      </div>
+    
+    <!-- Government badge -->
+    <td v-if="company.is_government" class="absolute text-xs top-2 right-2 p-2 bg-gray-600 text-black rounded-xl">
+      <h3>Government</h3>
     </td>
-    <td 
-      v-if="company.is_government"
-      class="absolute text-xs top-2 right-2 p-2 bg-gray-600 text-black rounded-xl"
-      ><h3>Government</h3></td
-    >
   </tr>
 </template>
-
 <style scoped>
   .shiny-button {
       position: relative;
