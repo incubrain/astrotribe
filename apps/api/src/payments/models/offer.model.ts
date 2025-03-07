a6fc53f156dd446259efc7049112f6ec565b4cfe
ruturajgole <ruturajpokemon1@gmail.com>
2025-03-07 08:05:08 +0000

    feat: add offers

---
+++ apps/api/src/payments/models/offer.model.ts
@@ -0,0 +1,20 @@
+// templates/entity/model.ejs
+import { BaseModel } from '@core'
+import { PlanModel } from './plan.model'
+import { DiscountPeriod, DiscountType } from '../../types/offer.types'
+
+// Model interface
+export interface OfferModel extends BaseModel {
+  id: string
+  is_active: boolean
+  created_at: Date
+  updated_at?: Date
+  discount: number
+  discount_type: DiscountType
+  discount_period: DiscountPeriod
+  already_discounted: boolean
+  expiry_date: Date
+
+  plan_id: number
+  plan: PlanModel
+}
---
+++ apps/api/src/types/offer.types.ts
@@ -0,0 +1,11 @@
+// types/offer.types.ejs
+export enum DiscountType {
+  PERCENTAGE = 'percentage',
+  FLAT = 'flat',
+}
+
+export enum DiscountPeriod {
+  YEARLY = 'yearly',
+  MONTHLY = 'monthly',
+  ONCE = 'once',
+}
