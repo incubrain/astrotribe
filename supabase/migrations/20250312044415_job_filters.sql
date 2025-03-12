ed6d99376bf57f5e8b85de943004320945b4a674
ruturajgole <ruturajpokemon1@gmail.com>
2025-03-12 05:03:42 +0000

    fix: update grants for the view job filters

--- supabase/migrations/20250312044415_job_filters.sql
+++ supabase/migrations/20250312044415_job_filters.sql
@@ -15,3 +15,33 @@ SELECT DISTINCT j.location,
   FROM jobs j
     JOIN companies c ON c.id = j.company_id
 ORDER BY c.name, j.location, j.employment_type;
+
+-- Change the owner of the view to "postgres"
+ALTER VIEW public.job_filters OWNER TO postgres;
+
+-- Grants for "authenticated" role
+GRANT DELETE ON public.job_filters TO authenticated;
+GRANT INSERT ON public.job_filters TO authenticated;
+GRANT REFERENCES ON public.job_filters TO authenticated;
+GRANT SELECT ON public.job_filters TO authenticated;
+GRANT TRUNCATE ON public.job_filters TO authenticated;
+GRANT UPDATE ON public.job_filters TO authenticated;
+
+-- Grants for "authenticator" role
+GRANT UPDATE ON public.job_filters TO authenticator;
+
+-- Grants for "service_role"
+GRANT DELETE ON public.job_filters TO service_role;
+GRANT INSERT ON public.job_filters TO service_role;
+GRANT REFERENCES ON public.job_filters TO service_role;
+GRANT SELECT ON public.job_filters TO service_role;
+GRANT TRUNCATE ON public.job_filters TO service_role;
+GRANT UPDATE ON public.job_filters TO service_role;
+
+-- Grants for "supabase_auth_admin"
+GRANT DELETE ON public.job_filters TO supabase_auth_admin;
+GRANT INSERT ON public.job_filters TO supabase_auth_admin;
+GRANT REFERENCES ON public.job_filters TO supabase_auth_admin;
+GRANT SELECT ON public.job_filters TO supabase_auth_admin;
+GRANT TRUNCATE ON public.job_filters TO supabase_auth_admin;
+GRANT UPDATE ON public.job_filters TO supabase_auth_admin;
