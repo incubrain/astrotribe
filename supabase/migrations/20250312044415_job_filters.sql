e72efd12aa6aa990b2f735198f95875af6cbe63d
ruturajgole <ruturajpokemon1@gmail.com>
2025-03-12 04:51:03 +0000

    fix: order of statements for migration

--- supabase/migrations/20250312044415_job_filters.sql
+++ supabase/migrations/20250312044415_job_filters.sql
@@ -6,6 +6,8 @@ SET employment_type_text = employment_type #>> '{}';
 
 ALTER TABLE jobs DROP COLUMN employment_type;
 
+ALTER TABLE jobs RENAME COLUMN employment_type_text TO employment_type;
+
 CREATE VIEW job_filters AS
 SELECT DISTINCT j.location,
   c.name AS company_name,
@@ -13,4 +15,3 @@ SELECT DISTINCT j.location,
   FROM jobs j
     JOIN companies c ON c.id = j.company_id
 ORDER BY c.name, j.location, j.employment_type;
-ALTER TABLE jobs RENAME COLUMN employment_type_text TO employment_type;
