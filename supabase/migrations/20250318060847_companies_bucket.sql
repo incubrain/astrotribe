df79e43d6a1ff4c2b16b9987e0ac7a872f76cfc3
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2025-03-23 14:43:54 +0530

    db: remove trailing , from companies_bucket.sql

--- supabase/migrations/20250318060847_companies_bucket.sql
+++ supabase/migrations/20250318060847_companies_bucket.sql
@@ -1,4 +1,4 @@
 INSERT INTO "storage"."buckets" 
 ("id", "name", "owner", "created_at", "updated_at", "public", "avif_autodetection", "file_size_limit", "allowed_mime_types", "owner_id") VALUES
-('companies', 'companies', NULL, '2025-03-18 05:39:17.641583+00', '2025-03-18 05:39:17.641583+00', true, false, NULL, NULL, NULL),
+('companies', 'companies', NULL, '2025-03-18 05:39:17.641583+00', '2025-03-18 05:39:17.641583+00', true, false, NULL, NULL, NULL)
 ON CONFLICT (id) DO NOTHING;
