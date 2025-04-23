f7a77d93f78eda7a66163c1fd139a64cf9e11971
ruturajgole <ruturajpokemon1@gmail.com>
2025-04-23 06:00:05 +0000

    log: try useRuntimeConfig for env

--- apps/website/components/nav/NavTop.vue
+++ apps/website/components/nav/NavTop.vue
@@ -24,6 +24,7 @@ const props = defineProps({
 
 // 5. Core Nuxt Composables
 const { $config } = useNuxtApp()
+console.log('USE RUNTIME CONFIG', useRuntimeConfig().public)
 console.log('Public Environment Variables', process.env.NODE_ENV)
 // 6. Other Composables
 const { y } = useWindowScroll()
