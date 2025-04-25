7be5ea9e88b63e102c7395ca3cb4236aa5a1445d
ruturajgole <ruturajpokemon1@gmail.com>
2025-04-25 08:39:48 +0000

    fix: show entire team in one row

--- apps/website/pages/team/index.vue
+++ apps/website/pages/team/index.vue
@@ -109,7 +109,7 @@ onMounted(() => {
 
         <!-- Grid of team member cards -->
         <div
-          class="grid w-full grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 md:gap-8 lg:gap-12 pb-6"
+          class="grid w-full grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4 md:gap-8 lg:gap-12 pb-6"
         >
           <TeamCard
             v-for="(member, index) in teamData"
